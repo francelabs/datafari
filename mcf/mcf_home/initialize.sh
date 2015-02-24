21728fb5e3875d1e1ea9adade6669a71741c192f
amazoyer <amazoyer@users.noreply.github.com>
2015-02-24 10:48:55 +0100

    Update initialize.sh

--- mcf/mcf_home/initialize.sh
+++ mcf/mcf_home/initialize.sh
@@ -18,7 +18,7 @@
 #Make sure environment variables are properly set
 if [ -e "$JAVA_HOME"/bin/java ] ; then
     if [ -f ./properties.xml ] ; then
-        ./executecommand-dev.sh org.apache.manifoldcf.crawler.InitializeAndRegister
+        ./executecommand.sh org.apache.manifoldcf.crawler.InitializeAndRegister
         exit $?
         
     else
