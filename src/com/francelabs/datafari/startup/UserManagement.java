9c85d2e7114ff8bcc485714fdfb7aa5b6bd4a61d
SABIR <sabiripb@gmail.com>
2015-08-25 09:40:26 +0200

    Correcting a bug

--- src/com/francelabs/datafari/startup/UserManagement.java
+++ src/com/francelabs/datafari/startup/UserManagement.java
@@ -15,25 +15,14 @@
  *******************************************************************************/
 package com.francelabs.datafari.startup;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.Semaphore;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 
-import com.francelabs.datafari.servlets.admin.StringsDatafariProperties;
-import com.francelabs.datafari.utils.ScriptConfiguration;
-import com.francelabs.datafari.utils.UpdateNbLikes;
-import com.francelabs.realm.MongoDBRunning;
-import com.francelabs.realm.User;
+import com.francelabs.datafari.constants.CodesReturned;
+import com.francelabs.datafari.user.User;
+
 
 /**
  * Servlet implementation class UserManagement
@@ -44,24 +33,24 @@ public class UserManagement extends HttpServlet {
 	private final static Logger LOGGER = Logger.getLogger(UserManagement.class.getName());
 	
 	public void init() throws ServletException{
-		
-		MongoDBRunning mongoDBRunning = new MongoDBRunning(User.IDENTIFIERSDB);
-		if (mongoDBRunning.isConnected()){
-			User admin = new User("admin","admin",mongoDBRunning.getDb());
-			if (admin.isInBase()){
-				if (admin.signIn()){
+			
+			User admin = new User("admin","admin");
+			admin.deleteUser();
+			int code = admin.isInBase();
+			if (code == CodesReturned.PROBLEMCONNECTIONDATABASE){
+				LOGGER.error("Mongodb is not running, please start it. UserManagement Failed to start");
+			}else if (code==CodesReturned.TRUE){
+				if (admin.signIn()==CodesReturned.TRUE){
 					LOGGER.info("WARNING : The account admin have the default password");
 				}
 			}else{
-				if (admin.signup("SearchAdministrator"))
+				if (admin.signup("SearchAdministrator")==CodesReturned.ALLOK)
 					LOGGER.info("The accound admin was created in database. Please change the password");
 				else{
 					LOGGER.error("Mongodb is not running, please start it. UserManagement Failed to start");
 				}
 			}
-		}else{
-			LOGGER.error("Mongodb is not running, please start it. UserManagement Failed to start");
-		}
+		
 	}
 	
 	/**
