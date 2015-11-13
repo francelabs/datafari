e4df58aee8aba74a63b10fa78298cbb595fcc351
amazoyer <amazoyer@francelabs.com>
2015-11-13 14:29:24 +0100

    Use lazyLoading for CassandraDBContextInitialization

--- src/com/francelabs/realm/CassandraDBContextListerner.java
+++ src/com/francelabs/realm/CassandraDBContextListerner.java
@@ -8,59 +8,62 @@ import org.apache.log4j.Logger;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-import com.francelabs.datafari.utils.ScriptConfiguration;
 
 /**
- * Application Lifecycle Listener implementation class CassandraDBContextListerner
+ * Application Lifecycle Listener implementation class
+ * CassandraDBContextListerner
  *
  */
 @WebListener
 public class CassandraDBContextListerner implements ServletContextListener {
 
+	private final static Logger LOGGER = Logger.getLogger(CassandraDBContextListerner.class.getName());
 
-	private final static Logger LOGGER = Logger
-			.getLogger(CassandraDBContextListerner.class.getName());
-	
 	private static final String host = "127.0.0.1";
-
 	private static final String KEYSPACE = "datafari";
 
-	private static Cluster cluster;
-	private static Session session;
-	
-	public static Session getSession(){
-		while  (session == null){
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				LOGGER.error("Unknow error");
+	private static Cluster cluster = null;
+	private static Session session = null;
+
+	public static synchronized Session getSession() {
+
+		try {
+			if (cluster != null || cluster.isClosed()) {
+				// Connect to the cluster and keyspace "demo"
+				cluster = Cluster.builder().addContactPoint(host).build();
+				LOGGER.info("Cassandra cluster initialized successfully");
+
 			}
+			if (session != null || session.isClosed()){
+				session = cluster.connect(KEYSPACE);
+				LOGGER.info("Cassandra" + KEYSPACE + " session initialized successfully");
+
+			}
+		} catch (Exception e) {
+			LOGGER.error("Error initializing Cassandra client", e);
 		}
+
 		return session;
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		if (cluster != null){
-		cluster.close();
-		LOGGER.info("Cassandra closed successfully");
+		if (session != null) {
+			session.close();
+			LOGGER.info("Cassandra" + KEYSPACE + " session closed successfully");
+		}
+
+		if (cluster != null) {
+			cluster.close();
+			LOGGER.info("Cassandra cluster closed successfully");
 		}
 
 		LOGGER.warn("Cassandra cluster was not initialized");
 	}
-	
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		try {
 
-			// Connect to the cluster and keyspace "demo"
-			cluster = Cluster.builder().addContactPoint(host).build();
-			session = cluster.connect(KEYSPACE);
-			LOGGER.info("Cassandra client initialized successfully");
-		} catch (Exception e) {
-			LOGGER.error("Error initializing Cassandra client", e);
-		}
 	}
-	
+
 }
