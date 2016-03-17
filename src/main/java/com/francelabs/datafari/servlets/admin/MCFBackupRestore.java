d06c073e265966a8f8c1eccc2d863f0a4dcc60f6
Giovanni USAI <giovanni.usai@francelabs.com>
2016-03-17 18:54:58 +0100

    [DATAFARI-123] MCF backup / restore UI, 1st commit

---
+++ src/main/java/com/francelabs/datafari/servlets/admin/MCFBackupRestore.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ *  * Copyright 2016 France Labs
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  * 
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *******************************************************************************/
+
+package com.francelabs.datafari.servlets.admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.francelabs.manifoldcf.configuration.script.BackupManifoldCFConnectorsScript;
+
+/**
+ * This Servlet is used to save and restore the MCF connections from AdminUI
+ * It is called by MCFBackupRestore.html
+ * DoGet is not used
+ * DoPost is used to save and restore the MCF connections, given the action parameter (save or restore)
+ * @author Giovanni Usai
+ */
+@WebServlet("/admin/MCFBackupRestore")
+public class MCFBackupRestore extends HttpServlet {
+	
+	private static final long serialVersionUID = -6561976993995634818L;
+
+	private String env;
+	
+	private static final String DEFAULT_MCF_BACKUP_DIR = "/bin/common/config/manifoldcf/bckp_restore";
+	
+	private final static Logger LOGGER = Logger.getLogger(MCFBackupRestore.class);
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 * Gets the environment path of Datafari installation
+	 */
+	public MCFBackupRestore() {
+		env = System.getenv("DATAFARI_HOME");									//Gets the directory of installation if in standard environment
+		if(env==null){															//If in development environment	
+			RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();	//Gets the D.solr.solr.home variable given in arguments to the VM
+			List<String> arguments = runtimeMxBean.getInputArguments();
+			for(String s : arguments){
+				if(s.startsWith("-Dsolr.solr.home"))
+					env = s.substring(s.indexOf("=")+1, s.indexOf("solr_home")-5);
+			}
+		}
+	}
+
+	/**
+	 * @throws IOException 
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * It saves the MCF connections if action parameter is save
+	 * It restores the MCF connections if action parameter is restore
+	 * It uses the backup directory in input (if specified) or a default path
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			
+			String action = request.getParameter("action");
+			
+			if (action != null && !action.trim().isEmpty()){
+				
+				String backupDirTmp = request.getParameter("backup_dir");
+				
+				String backupDirectory;
+				
+				// Check if the backup dir has been given in input, otherwise take the default path
+				if (backupDirTmp != null && !backupDirTmp.trim().isEmpty()){
+					backupDirectory = backupDirTmp.trim();
+				} else {
+					backupDirectory = env + DEFAULT_MCF_BACKUP_DIR;
+				}
+				
+				if (action.trim().equalsIgnoreCase("save")){
+					
+					BackupManifoldCFConnectorsScript.doSave(backupDirectory);
+					
+				} else if (action.trim().equalsIgnoreCase("restore")){
+					
+					BackupManifoldCFConnectorsScript.doRestore(backupDirectory);
+				}
+			}
+		} catch(Exception e){
+			PrintWriter out = response.getWriter();
+			out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69253");
+			out.close();
+			LOGGER.error("Error in MCFBackupRestore doPost. Error 69253", e);
+		}
+	}
+}
