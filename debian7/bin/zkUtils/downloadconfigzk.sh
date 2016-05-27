854b782db31d026e099550a4add2d18d19fa4cfc
otavard <gitoliv@francelabs.com>
2016-05-27 09:19:11 +0200

    Scripts ZK updated

--- debian7/bin/zkUtils/downloadconfigzk.sh
+++ debian7/bin/zkUtils/downloadconfigzk.sh
@@ -5,5 +5,5 @@
 #
 #
 
-"$1solr/server/scripts/cloud-scripts/zkcli.sh" -zkhost $2 -cmd downconfig -confname $3 -confdir $1solr/solrcloud/tmp/
+"$1/solr/server/scripts/cloud-scripts/zkcli.sh" -zkhost $2 -cmd downconfig -confname $3 -confdir $1/solr/solrcloud/tmp/
 
--- debian7/bin/zkUtils/uploadconfigzk.sh
+++ debian7/bin/zkUtils/uploadconfigzk.sh
@@ -7,7 +7,7 @@
 
 
 
-"$1solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost $2 -confdir $1solr/solrcloud/tmp/ -confname $3
+"$1/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost $2 -confdir $1/solr/solrcloud/tmp/ -confname $3
 
 rm -rf $1solr/solrcloud/tmp/
 
