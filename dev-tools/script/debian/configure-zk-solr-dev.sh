b448bdc6f15145b7ef76d18eecaf181dac36d657
otavard <gitoliv@francelabs.com>
2016-06-01 17:33:28 +0200

    Postinst cleaned, dev tools added

--- dev-tools/script/debian/configure-zk-solr-dev.sh
+++ dev-tools/script/debian/configure-zk-solr-dev.sh
@@ -10,6 +10,16 @@ DIR=../../../debian7/bin
 source "set-datafari-env-devmode.sh"
 source "${DIR}/utils.sh"
 
+
+sed -i -e "s/@NODEHOST@/localhost/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+sed -i -e "s/@NODEHOST@/localhost/g" ${DATAFARI_HOME}/solr/bin/solr.in.sh
+sed -i -e "s/@SOLRHOSTS@/localhost:2181/g" ${DATAFARI_HOME}/solr/bin/solr.in.sh
+sed -i -e "s/@SOLRHOSTS@/localhost:2181/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+
+
+sed -i -e "s/@NUMSHARDS@/1/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+sed -i -e "s/@NUMSHARDS@/1/g" ${DATAFARI_HOME}/bin/start-datafari.sh
+sed -i -e "s/@ISMAINNODE@/true/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
 mkdir $DATAFARI_HOME/solr/solrcloud
 mv ${DATAFARI_HOME}/solr/solr_home/FileShare ${DATAFARI_HOME}/solr/solrcloud
 mv ${DATAFARI_HOME}/solr/solr_home/Statistics ${DATAFARI_HOME}/solr/solrcloud
