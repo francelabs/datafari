90064e197ddfc491c84e015c2abb97364fd5ef8e
amazoyer <aurelien.mazoyer@francelabs.com>
2016-06-15 19:23:30 +0200

    Add Tika connection for MCF with OCR activated Remove OCR from Solr ExtractingHandler Enable OCR of PDF Fix OCR for Ubuntu version >= 14.04 Fix no DatafariAuthorityGroup found Exception when connecting to an Active Directory in the UI Fix BackupMCFConnectors first start exception log

--- debian7/bin/set-datafari-env.sh
+++ debian7/bin/set-datafari-env.sh
@@ -8,14 +8,13 @@
 export DATAFARI_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export JAVA_HOME=${DATAFARI_HOME}/jvm
-
-echo $JAVA_HOME
-
-export LD_LIBRARY_PATH=${DATAFARI_HOME}/pgsql/lib:${DATAFARI_HOME}/ocr/tesseract/lib:${DATAFARI_HOME}/ocr/leptonica/lib
 export INIT_STATE_FILE=${DATAFARI_HOME}/bin/common/init_state.properties
 export CONFIG_FILE=${DATAFARI_HOME}/tomcat/conf/datafari.properties
-export TESSDATA_PREFIX=${DATAFARI_HOME}/ocr/tesseract/share
-export LC_NUMERIC=C
+
+
+
+
+
 export PID_DIR=$DATAFARI_HOME/pid
 export TOMCAT_HOME=$DATAFARI_HOME/tomcat
 export MCF_HOME=$DATAFARI_HOME/mcf/mcf_home
