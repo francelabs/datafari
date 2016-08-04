826d021928863cebe0cda5020f9247a9ecfbb2ad
otavard <gitoliv@francelabs.com>
2016-08-04 14:06:15 +0200

    Bug fix script dev

--- dev-tools/script/dev/start-datafari-devmode.sh
+++ dev-tools/script/dev/start-datafari-devmode.sh
@@ -37,6 +37,21 @@ fi
 if  [[ "$STATE" = *installed* ]];
 then
 
+# Configuration Solr and ZK
+sed -i -e "s/@NODEHOST@/localhost/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+sed -i -e "s/@NODEHOST@/localhost/g" ${DATAFARI_HOME}/solr/bin/solr.in.sh
+sed -i -e "s/@SOLRHOSTS@/localhost:2181/g" ${DATAFARI_HOME}/solr/bin/solr.in.sh
+sed -i -e "s/@SOLRHOSTS@/localhost:2181/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+sed -i -e "s/@NUMSHARDS@/1/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+sed -i -e "s/@ISMAINNODE@/true/g" ${DATAFARI_HOME}/tomcat/conf/datafari.properties
+mkdir $DATAFARI_HOME/solr/solrcloud
+chmod -R 777 ${DATAFARI_HOME}/solr/solrcloud
+mv ${DATAFARI_HOME}/solr/solr_home/FileShare ${DATAFARI_HOME}/solr/solrcloud
+mv ${DATAFARI_HOME}/solr/solr_home/Statistics ${DATAFARI_HOME}/solr/solrcloud
+mv ${DATAFARI_HOME}/solr/solr_home/Promolink ${DATAFARI_HOME}/solr/solrcloud
+chmod -R 777 ${DATAFARI_HOME}/pid
+
+
 echo "First launch"
 # Start ZK
 
