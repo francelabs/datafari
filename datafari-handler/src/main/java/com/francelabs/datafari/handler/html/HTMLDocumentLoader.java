3be373e5836651d1fe70cbb287a25f8513321fe7
amazoyer <amazoyer@users.noreply.github.com>
2016-12-20 13:20:31 +0100

    Refacto HTMLDocumentLoader + add comments

--- datafari-handler/src/main/java/com/francelabs/datafari/handler/html/HTMLDocumentLoader.java
+++ datafari-handler/src/main/java/com/francelabs/datafari/handler/html/HTMLDocumentLoader.java
@@ -1,8 +1,7 @@
 package com.francelabs.datafari.handler.html;
 
-
 import java.io.InputStream;
-
+import java.lang.invoke.MethodHandles;
 
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.SolrParams;
@@ -16,22 +15,24 @@ import org.apache.solr.update.processor.UpdateRequestProcessor;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class HTMLDocumentLoader extends ContentStreamLoader{
+public class HTMLDocumentLoader extends ContentStreamLoader {
 
-	public static final String FIELD_PARAM = "field";
-	public static final String SELECTOR_PARAM = "selector";
-	public static final String SEPARATOR_PARAM = "separator";
-	public static final String DEFAULT_SEPARATOR = "";
+	private static final String FIELD_PARAM = "field";
+	private static final String SELECTOR_PARAM = "selector";
+	private static final String SEPARATOR_PARAM = "separator";
+	private static final String DEFAULT_SEPARATOR = "";
 
-	public String[] fields;
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	private String[] fields;
 
 	@Override
-	public void load(SolrQueryRequest req, SolrQueryResponse rsp,
-			ContentStream stream, UpdateRequestProcessor processor)
-					throws Exception {
+	public void load(SolrQueryRequest req, SolrQueryResponse rsp, ContentStream stream,
+			UpdateRequestProcessor processor) throws Exception {
 
 		SolrParams params = req.getParams();
 		AddUpdateCommand templateAdd = new AddUpdateCommand(req);
@@ -43,87 +44,66 @@ public class HTMLDocumentLoader extends ContentStreamLoader{
 		Document document = Jsoup.parse(is, "UTF-8", "");
 
 		fields = params.getParams(FIELD_PARAM);
-		for (String field : fields){
-			if (field.startsWith("ATTR")){
+		
+		// for each fields in the request handler configuration, fetch the data that matches
+		// the selector (also specified in request handler) through Jsoup and put it the solr input document
+		for (String field : fields) {
+			if (field.startsWith("ATTR") || field.startsWith("KEY")) {
 				String selector = params.getFieldParam(field, SELECTOR_PARAM);
 				String separator = params.getFieldParam(field, SEPARATOR_PARAM);
-				if (separator == null){
+				if (separator == null) {
 					separator = DEFAULT_SEPARATOR;
 				}
 
 				try {
 					Elements elements = document.select(selector);
 					StringBuilder sBuilder = new StringBuilder();
-					for (int i=0; i<elements.size(); i++){
-						sBuilder.append(elements.get(i).attr("class"));
-						if (i<elements.size()-1)
-							sBuilder.append(separator);
+					for (int i = 0; i < elements.size(); i++) {
+						if (field.startsWith("ATTR")) {
+							sBuilder.append(elements.get(i).attr("class"));
+							if (i < elements.size() - 1)
+								sBuilder.append(separator);
+							templateAdd.solrDoc.addField(field.replaceAll("ATTR", ""), sBuilder.toString());
+						} else {
+							sBuilder.append(elements.get(i).attr("content"));
+							if (i < elements.size() - 1)
+								sBuilder.append(separator);
+							templateAdd.solrDoc.addField(field.replaceAll("KEY", ""), sBuilder.toString());
+						}
+
 					}
-					templateAdd.solrDoc.addField(field.replaceAll("ATTR", ""), sBuilder.toString());
+				} catch (Exception e) {
+					logger.error("cannot extract data : " + e.getMessage(), e);
 				}
-				catch(Exception e){
-					System.out.println("cannot extract data : "+e.getMessage());
-					e.printStackTrace();
-				}
-			}
-			else if (field.startsWith("KEY")){
+			} else {
 				String selector = params.getFieldParam(field, SELECTOR_PARAM);
 				String separator = params.getFieldParam(field, SEPARATOR_PARAM);
-				if (separator == null){
+				if (separator == null) {
 					separator = DEFAULT_SEPARATOR;
 				}
 
 				try {
 					Elements elements = document.select(selector);
 					StringBuilder sBuilder = new StringBuilder();
-					for (int i=0; i<elements.size(); i++){
-						sBuilder.append(elements.get(i).attr("content"));
-						if (i<elements.size()-1)
-							sBuilder.append(separator);
-					}
-					templateAdd.solrDoc.addField(field.replaceAll("KEY", ""), sBuilder.toString());
-					System.out.println("keywords"+ sBuilder.toString());
-				}
-				catch(Exception e){
-					System.out.println("cannot extract data : "+e.getMessage());
-					e.printStackTrace();
-				}
-			}
-
-
-			else {
-				String selector = params.getFieldParam(field, SELECTOR_PARAM);
-				String separator = params.getFieldParam(field, SEPARATOR_PARAM);
-				if (separator == null){
-					separator = DEFAULT_SEPARATOR;
-				}
-
-				try {
-					Elements elements = document.select(selector);
-					StringBuilder sBuilder = new StringBuilder();
-					for (int i=0; i<elements.size(); i++){
+					for (int i = 0; i < elements.size(); i++) {
 						sBuilder.append(elements.get(i).text());
-						if (i<elements.size()-1)
+						if (i < elements.size() - 1)
 							sBuilder.append(separator);
 					}
 					templateAdd.solrDoc.addField(field, sBuilder.toString());
-				}
-				catch(Exception e){
-					System.out.println("cannot extract data : "+e.getMessage());
-					e.printStackTrace();
+				} catch (Exception e) {
+					logger.error("cannot extract data : " + e.getMessage(), e);
+
 				}
 			}
 
 		}
 
 		try {
-			templateAdd.solrDoc.addField("id",req.getParams().get("literal.id"));
+			templateAdd.solrDoc.addField("id", req.getParams().get("literal.id"));
+		} catch (Exception e) {
+			logger.error("cannot extract data : " + e.getMessage(), e);
 		}
-		catch(Exception e){
-			System.out.println("cannot extract data : "+e.getMessage());
-			e.printStackTrace();
-		}
-
 
 		IOUtils.closeQuietly(is);
 
