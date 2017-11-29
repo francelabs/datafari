a304394dbc46a1511ae929e0ea6059f9e6b2dd8e
jmassiera <gitjulien@francelabs.com>
2017-11-29 10:15:15 +0100

    Removed servlet semaphores

--- datafari-core/src/main/java/com/francelabs/datafari/servlets/admin/ModifyNodeContent.java
+++ datafari-core/src/main/java/com/francelabs/datafari/servlets/admin/ModifyNodeContent.java
@@ -18,8 +18,6 @@ package com.francelabs.datafari.servlets.admin;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -59,259 +57,200 @@ import com.francelabs.datafari.utils.ZKUtils;
  */
 @WebServlet("/admin/ModifyNodeContent")
 public class ModifyNodeContent extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private final String server = Core.FILESHARE.toString();
-	private static final List<SemaphoreLn> listMutex = new ArrayList<SemaphoreLn>();
-	private final String env;
-	private Document doc;
-	private File config = null;
-	private final static Logger LOGGER = Logger.getLogger(ModifyNodeContent.class.getName());
+  private static final long serialVersionUID = 1L;
+  private final String server = Core.FILESHARE.toString();
+  private final String env;
+  private Document doc;
+  private File config = null;
+  private final static Logger LOGGER = Logger.getLogger(ModifyNodeContent.class.getName());
 
-	/**
-	 * @see HttpServlet#HttpServlet() Gets the path Checks if the required file
-	 *      exist
-	 */
-	public ModifyNodeContent() {
-		String environnement = Environment.getEnvironmentVariable("DATAFARI_HOME");
+  /**
+   * @see HttpServlet#HttpServlet() Gets the path Checks if the required file
+   *      exist
+   */
+  public ModifyNodeContent() {
+    String environnement = Environment.getEnvironmentVariable("DATAFARI_HOME");
 
-		if (environnement == null) { // If in development environment
-			environnement = ExecutionEnvironment.getDevExecutionEnvironment();
-		}
-		env = environnement + "/solr/solrcloud/FileShare/conf";
+    if (environnement == null) { // If in development environment
+      environnement = ExecutionEnvironment.getDevExecutionEnvironment();
+    }
+    env = environnement + "/solr/solrcloud/FileShare/conf";
 
-		if (new File(env + "/solrconfig.xml").exists()) {
-			config = new File(env + "/solrconfig.xml");
-		}
-	}
+    if (new File(env + "/solrconfig.xml").exists()) {
+      config = new File(env + "/solrconfig.xml");
+    }
+  }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response) Used to release the semaphore Or create and or acquire the
-	 *      semaphore, then read the file to get the requested node
-	 */
-	@Override
-	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
-		SemaphoreLn acquiredSem = null;
-		try {
-			final String type = request.getParameter("type");
-			try {
-				if (request.getParameter("sem") != null) { // If it's called
-															// just to clean the
-															// semaphore
-					for (int i = 0; i < listMutex.size(); i++) { // Get the one
-																	// corresponding
-																	// to the
-																	// parameter
-																	// "type"
-																	// and
-																	// checks if
-																	// it has
-																	// not
-																	// already
-																	// been
-																	// released
-						if (listMutex.get(i).getType().equals(type) && (listMutex.get(i).availablePermits() < 1)) {
-							listMutex.get(i).release();
-							return;
-						}
-					}
-					return;
-				}
-				if ((config == null) || !new File(env + "/solrconfig.xml").exists()) {// If
-																						// the
-																						// file
-																						// did
-																						// not
-																						// existed
-																						// when
-																						// the
-																						// constructor
-																						// was
-																						// run
-					// Checks if it exists now
-					if (!new File(env + "/solrconfig.xml").exists()) {
-						LOGGER.error(
-								"Error while opening the configuration file, solrconfig.xml, in ModifyNodeContent doGet, please make sure this file exists at "
-										+ env + "/solr/solrcloud/" + server + "/conf/ . Error 69033"); // If
-																										// not
-																										// an
-																										// error
-																										// is
-																										// printed
-						final PrintWriter out = response.getWriter();
-						out.append(
-								"Error while opening the configuration file, please retry, if the problem persists contact your system administrator. Error Code : 69033");
-						out.close();
-						return;
-					} else {
-						config = new File(env + "/solrconfig.xml");
-					}
-				}
-				boolean mutex = false;
-				for (int i = 0; i < listMutex.size(); i++) { // If the right
-																// semaphore
-																// exists and is
-																// available,
-																// acquire it.
-					if (listMutex.get(i).getType().equals(type)) {
-						if (listMutex.get(i).availablePermits() > 0) {
-							listMutex.get(i).acquire();
-							acquiredSem = listMutex.get(i);
-						} else { // If not available return "File already in
-									// use"
-							final PrintWriter out = response.getWriter();
-							out.append("File already in use");
-							out.close();
-							return;
-						}
-						mutex = true;
-					}
-				}
-				if (!mutex) { // If not existing then create it and acquire it
-					listMutex.add(new SemaphoreLn("", type));
-					acquiredSem = listMutex.get(listMutex.size() - 1);
-					acquiredSem.acquire();
-				}
-				final String attr = request.getParameter("attr");
-				final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-				final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-				doc = dBuilder.parse(config); // Parse the solrconfig.xml
-				final NodeList childNodes = doc.getChildNodes();
-				final Element elem = (Element) run(childNodes, type, attr); // Search
-																			// for
-																			// the
-																			// requested
-																			// node
-				final PrintWriter out = response.getWriter();
-				out.append(elem.getTextContent()); // Return it's content
-				out.close();
-			} catch (ParserConfigurationException | SAXException e) {
-				LOGGER.error(
-						"Error while parsing the solrconfig.xml, in ModifyNodeContent doGet, make sure the file is valid. Error 69034",
-						e);
-				final PrintWriter out = response.getWriter();
-				out.append(
-						"Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69034");
-				out.close();
-				if (acquiredSem != null) {
-					acquiredSem.release();
-				}
-				return;
-			}
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response) Used to release the semaphore Or create and or acquire the
+   *      semaphore, then read the file to get the requested node
+   */
+  @Override
+  protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+    final SemaphoreLn acquiredSem = null;
+    try {
+      final String type = request.getParameter("type");
+      try {
+        if (request.getParameter("sem") != null) { // If it's called
+          // just to clean the
+          // semaphore
+          return;
+        }
+        if ((config == null) || !new File(env + "/solrconfig.xml").exists()) {// If
+          // the
+          // file
+          // did
+          // not
+          // existed
+          // when
+          // the
+          // constructor
+          // was
+          // run
+          // Checks if it exists now
+          if (!new File(env + "/solrconfig.xml").exists()) {
+            LOGGER
+                .error("Error while opening the configuration file, solrconfig.xml, in ModifyNodeContent doGet, please make sure this file exists at "
+                    + env + "/solr/solrcloud/" + server + "/conf/ . Error 69033"); // If
+            // not
+            // an
+            // error
+            // is
+            // printed
+            final PrintWriter out = response.getWriter();
+            out.append(
+                "Error while opening the configuration file, please retry, if the problem persists contact your system administrator. Error Code : 69033");
+            out.close();
+            return;
+          } else {
+            config = new File(env + "/solrconfig.xml");
+          }
+        }
 
-		} catch (final Exception e) {
-			final PrintWriter out = response.getWriter();
-			out.append(
-					"Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69514");
-			out.close();
-			LOGGER.error("Unindentified error in ModifyNodeContent doGet. Error 69514", e);
-			if (acquiredSem != null) {
-				acquiredSem.release();
-			}
-		}
-	}
+        final String attr = request.getParameter("attr");
+        final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        doc = dBuilder.parse(config); // Parse the solrconfig.xml
+        final NodeList childNodes = doc.getChildNodes();
+        final Element elem = (Element) run(childNodes, type, attr); // Search
+        // for
+        // the
+        // requested
+        // node
+        final PrintWriter out = response.getWriter();
+        out.append(elem.getTextContent()); // Return it's content
+        out.close();
+      } catch (ParserConfigurationException | SAXException e) {
+        LOGGER.error("Error while parsing the solrconfig.xml, in ModifyNodeContent doGet, make sure the file is valid. Error 69034", e);
+        final PrintWriter out = response.getWriter();
+        out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69034");
+        out.close();
+        if (acquiredSem != null) {
+          acquiredSem.release();
+        }
+        return;
+      }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response) Read the file and search for the requested node, then set
-	 *      it's textContent to the parameter
-	 */
-	@Override
-	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
-			throws ServletException, IOException {
-		try {
-			final String type = request.getParameter("type");
-			final String value = request.getParameter("value");
-			final String attr = request.getParameter("attr");
-			Element elem;
-			String oldNodeStr = null;
-			try {
-				final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-				final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-				doc = dBuilder.parse(config); // Parse the solrconfig.xml
-				final NodeList childNodes = doc.getChildNodes();
-				elem = (Element) run(childNodes, type, attr); // Get the
-																// requested
-																// Node
-				oldNodeStr = XMLUtils.nodeToString(elem);
-			} catch (ParserConfigurationException | SAXException e) {
-				LOGGER.error(
-						"Error while parsing the solrconfig.xml, in ModifyNodeContent doPost, make sure the file is valid. Error 69035",
-						e);
-				final PrintWriter out = response.getWriter();
-				out.append(
-						"Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69035");
-				out.close();
-				return;
-			}
-			try {
-				elem.setTextContent(value); // Set the value of the node
-				final String newNodeStr = XMLUtils.nodeToString(elem);
-				String configStr = FileUtils.getFileContent(config);
-				configStr = configStr.replace(oldNodeStr, newNodeStr);
-				FileUtils.saveStringToFile(config, configStr); // Modify the
-																// file
-				for (int i = 0; i < listMutex.size(); i++) { // Release the
-																// semaphore
-					if (listMutex.get(i).getType().equals(type) && (listMutex.get(i).availablePermits() < 1)) {
-						listMutex.get(i).release();
-					}
-				}
-			} catch (final TransformerException e) {
-				LOGGER.error("Error while modifying the solrconfig.xml, in ModifyNodeContent doPost. Error 69036", e);
-				final PrintWriter out = response.getWriter();
-				out.append(
-						"Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69036");
-				out.close();
-				return;
-			}
+    } catch (final Exception e) {
+      final PrintWriter out = response.getWriter();
+      out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69514");
+      out.close();
+      LOGGER.error("Unindentified error in ModifyNodeContent doGet. Error 69514", e);
+      if (acquiredSem != null) {
+        acquiredSem.release();
+      }
+    }
+  }
 
-			ZKUtils.configZK("uploadconfigzk.sh", "FileShare");
-			ZKUtils.configZK("reloadCollections.sh", "FileShare");
-		} catch (final Exception e) {
-			final PrintWriter out = response.getWriter();
-			out.append(
-					"Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69515");
-			out.close();
-			LOGGER.error("Unindentified error in ModifyNodeContent doPost. Error 69515", e);
-		}
-	}
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response) Read the file and search for the requested node, then set
+   *      it's textContent to the parameter
+   */
+  @Override
+  protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+    try {
+      final String type = request.getParameter("type");
+      final String value = request.getParameter("value");
+      final String attr = request.getParameter("attr");
+      Element elem;
+      String oldNodeStr = null;
+      try {
+        final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        doc = dBuilder.parse(config); // Parse the solrconfig.xml
+        final NodeList childNodes = doc.getChildNodes();
+        elem = (Element) run(childNodes, type, attr); // Get the
+        // requested
+        // Node
+        oldNodeStr = XMLUtils.nodeToString(elem);
+      } catch (ParserConfigurationException | SAXException e) {
+        LOGGER.error("Error while parsing the solrconfig.xml, in ModifyNodeContent doPost, make sure the file is valid. Error 69035", e);
+        final PrintWriter out = response.getWriter();
+        out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69035");
+        out.close();
+        return;
+      }
+      try {
+        elem.setTextContent(value); // Set the value of the node
+        final String newNodeStr = XMLUtils.nodeToString(elem);
+        String configStr = FileUtils.getFileContent(config);
+        configStr = configStr.replace(oldNodeStr, newNodeStr);
+        FileUtils.saveStringToFile(config, configStr); // Modify the
+        // file
+      } catch (final TransformerException e) {
+        LOGGER.error("Error while modifying the solrconfig.xml, in ModifyNodeContent doPost. Error 69036", e);
+        final PrintWriter out = response.getWriter();
+        out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69036");
+        out.close();
+        return;
+      }
 
-	private Node run(final NodeList child, final String type, final String attr) { // Function
-																					// to
-																					// search
-																					// for
-																					// a
-																					// node
-																					// by
-																					// it's
-																					// attribute
-																					// "name"
-																					// in
-																					// a
-																					// childList
-																					// and
-		for (int i = 0; i < child.getLength(); i++) {
-			String name = "";
-			if (child.item(i).hasAttributes()) {
-				final NamedNodeMap map = child.item(i).getAttributes();
-				for (int j = 0; j < map.getLength(); j++) {
-					if (map.item(j).getNodeName().equals(attr)) {
-						name = map.item(j).getNodeValue();
-					}
-				}
-				if (name.equals(type)) {
-					return child.item(i);
-				}
-			}
-			if (child.item(i).hasChildNodes()) {
-				if (run(child.item(i).getChildNodes(), type, attr) != null) {
-					return run(child.item(i).getChildNodes(), type, attr);
-				}
-			}
-		}
-		return null;
-	}
+      ZKUtils.configZK("uploadconfigzk.sh", "FileShare");
+      ZKUtils.configZK("reloadCollections.sh", "FileShare");
+    } catch (final Exception e) {
+      final PrintWriter out = response.getWriter();
+      out.append("Something bad happened, please retry, if the problem persists contact your system administrator. Error code : 69515");
+      out.close();
+      LOGGER.error("Unindentified error in ModifyNodeContent doPost. Error 69515", e);
+    }
+  }
+
+  private Node run(final NodeList child, final String type, final String attr) { // Function
+    // to
+    // search
+    // for
+    // a
+    // node
+    // by
+    // it's
+    // attribute
+    // "name"
+    // in
+    // a
+    // childList
+    // and
+    for (int i = 0; i < child.getLength(); i++) {
+      String name = "";
+      if (child.item(i).hasAttributes()) {
+        final NamedNodeMap map = child.item(i).getAttributes();
+        for (int j = 0; j < map.getLength(); j++) {
+          if (map.item(j).getNodeName().equals(attr)) {
+            name = map.item(j).getNodeValue();
+          }
+        }
+        if (name.equals(type)) {
+          return child.item(i);
+        }
+      }
+      if (child.item(i).hasChildNodes()) {
+        if (run(child.item(i).getChildNodes(), type, attr) != null) {
+          return run(child.item(i).getChildNodes(), type, attr);
+        }
+      }
+    }
+    return null;
+  }
 
 }
