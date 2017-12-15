7c771a7e8ffbfaf29ab10b54edb8100dc6f2e1d7
Romaric Pighetti <romaric.pighetti@francelabs.com>
2017-12-15 17:47:23 +0100

    DATAFARI-225-Improve_add_user_interface
    Add user form has a better look and feel.
    Cleaned up javascript code, improved verification and internationalization added.

--- datafari-core/src/main/webapp/ajax/js/addUser.js
+++ datafari-core/src/main/webapp/ajax/js/addUser.js
@@ -9,10 +9,10 @@ var clearTimeouts = function() {
 	for (var i = 0; i < timeouts.length; i++) {
 	    clearTimeout(timeouts[i]);
 	}
-	
+
 	//quick reset of the timer array you just cleared
 	timeouts = [];
-	
+
 	$('#main').unbind('DOMNodeRemoved');
 }
 
@@ -34,42 +34,39 @@ $(document).ready(function(){
 	var admin_messageDiv = $("#Message");
 	var ENDOFANIMATION = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 	var listRoles = ["SearchAdministrator","SearchExpert","ConnectedSearchUser"];
-	
+
 	var username;
 	var password;
 	var bLDAPUser = false;
 	var error=[];
 	var sourceError;
 	timeouts.push(setTimeout(function(){$('input[type="text"],input[type="password"]').val("");},200));
-	
+
 	setupLanguage();
-	
-	$("#ldapUserLabel").click(function() {
-		if($("#ldapUserLabel input").is(':checked')) {
-			bLDAPUser = true;
-			$("#password").hide();
-			$("#confirmPassword").hide();
-			$("#password").val('');
-			$("#confirmPassword").val('');
-			error["password"]={};
-			error["password"].isError = false;
-			error["confirmPassword"]={};
-			error["confirmPassword"].isError = false;
-		} else {
-			bLDAPUser = false;
-			$("#password").show();
-			$("#confirmPassword").show();
-		}
-	});
- 	
+
  	function setupLanguage(){
  		$(document).find('a.indexAdminUIBreadcrumbLink').prop('href', "./index.jsp?lang=" + window.i18n.language);
  		$("#topbar1").text(window.i18n.msgStore['home']);
  		$("#topbar2").text(window.i18n.msgStore['adminUI-UserManagement']);
  	 	$("#topbar3").text(window.i18n.msgStore['adminUI-AddUser']);
+ 	 	$("#title").text(window.i18n.msgStore['adminUI-AddUser']);
+ 	 	$("#username-label").text(window.i18n.msgStore['username']);
+ 	 	$("#username-input").attr("placeholder", window.i18n.msgStore['username']);
+ 	 	$("#username-error-text").text(window.i18n.msgStore['adminUI-UsernameAlreadyUsed']);
+ 	 	$("#username-warning-text").text(window.i18n.msgStore['adminUI-UsernameInvalid']);
+ 	 	$("#ldap-user-label").text(window.i18n.msgStore['adminUI-CreateUserFromAD']);
+ 	 	$("#password-label").text(window.i18n.msgStore['adminUI-Password']);
+ 	 	$("#password-input").attr("placeholder", window.i18n.msgStore['adminUI-Password']);
+ 	 	$("#confirm-password-label").text(window.i18n.msgStore['confirmPassword']);
+ 	 	$("#confirm-password-input").attr("placeholder", window.i18n.msgStore['adminUI-Password']);
+ 	 	$("#password-error-text").text(window.i18n.msgStore['passProblem']);
+ 	 	$("#roles-label").text(window.i18n.msgStore['adminUI-AddRoles']);
+ 	 	$("#roles-error-text").text(window.i18n.msgStore['adminUI-RolesError']);
+ 	 	$("#add-button").text(window.i18n.msgStore['adminUI-AddUser']);
+ 	 	$("#add-button").attr('data-loading-text', "<i class='fa fa-spinner fa-spin'></i> " + window.i18n.msgStore['adminUI-AddUser']);
  	}
- 	
- 	
+
+
 	function htmlRole(role){
 		return '<div class="inline_block"><div class="input-group role">'+
 		  '<span class="input-group-addon delete"><i class="fa fa-times"></i></span>'+
@@ -85,7 +82,7 @@ $(document).ready(function(){
 					danger = false;
 					message = window.i18n.msgStore["NOFAVORITESFOUND"];
 					break;
-				case SERVERNOTCONNECTED:	
+				case SERVERNOTCONNECTED:
 					message = window.i18n.msgStore["SERVERNOTCONNECTED"];
 					break;
 				case SERVERPROBLEMCONNECTIONDB:
@@ -102,11 +99,11 @@ $(document).ready(function(){
 					hide = false;
 					message = "username already used";
 					break;
-				case FIELDNOTFILLED : 
+				case FIELDNOTFILLED :
 					hide = false;
 					message = "Fields not all filled";
 					break;
-				case PROBLEMCONNECTIONAD: 
+				case PROBLEMCONNECTIONAD:
 					hide = false;
 					message = window.i18n.msgStore["PROBLEMCONNECTIONAD"];
 					break;
@@ -133,105 +130,178 @@ $(document).ready(function(){
 			}
 	}
 	
-	$(".input").blur(function(e){
-		var element = $(e.target);
-		var attribute = element.attr("name");
-		
-		switch(attribute){
-			case "username":
-				if (element.val()!=""){
-					$.post("../SearchAdministrator/isUserInBase",{username:element.val()},function(data){
-						if (data.code == 0){
-							element.next().hide();
-							if (data.status == "true"){
-								error["username"].isError = true;
-								element.next().show();
-								showError(USERALREADYINBASE,attribute);
-							}else{
-								error["username"]={};
-								error["username"].isError = false;
-								username = element.val();
-								if (sourceError == attribute){
-									admin_messageDiv.hide();
-								}
-							}
-							
-						}else{
-							error["username"].isError = true;
-							element.next().show();
-							showError(data.code,"username");
-						}
-					},"json");
-					username = element.val();
-				}
-				break;
-			case "password":
-				if (element.val()!=""){
-					element.next().hide();
-					error["password"]={};
-					error["password"].isError = false;
-					if (sourceError == attribute){
-						admin_messageDiv.hide();
-					}
-					var confirmPassword;
-					if ((confirmPassword = $('input[name="confirmPassword"]').val()) !="" && confirmPassword!=element.val()){
-						error["password"].isError = true;
-						$('input[name="confirmPassword"]').next().show();
-						showError(CONFIRMPASSWORDNOTCORRECT,"confirmPassword");
+	function clearStatus(element) {
+		let parent = element.closest(".form-group"); 
+		parent.removeClass("has-error");
+		parent.removeClass("has-warning");
+		parent.removeClass("has-success");
+		parent.removeClass("has-feedback");
+		let glyphElement = element.next();
+		glyphElement.removeClass("glyphicon-remove");
+		glyphElement.removeClass("glyphicon-warning-sign");
+		glyphElement.removeClass("glyphicon-ok");
+	};
+	
+	function setSuccess(element) {
+		element.closest(".form-group").addClass("has-success");
+		element.closest(".form-group").addClass("has-feedback");
+		element.next().addClass("glyphicon-ok");
+	};
+	
+	function setWarning(element) {
+		element.closest(".form-group").addClass("has-warning");
+		element.closest(".form-group").addClass("has-feedback");
+		element.next().addClass("glyphicon-warning-sign");
+	}
+	
+	function setError(element) {
+		element.closest(".form-group").addClass("has-error");
+		element.closest(".form-group").addClass("has-feedback");
+		element.next().addClass("glyphicon-remove");
+	};
+	
+	$("#username-input").blur(function(e) {
+		let element = $(e.target);
+		clearStatus(element);
+		//$("#username-tip-text").text("")
+		if (element.val()!=""){
+			$.post("../SearchAdministrator/isUserInBase",{username:element.val()},function(data){
+				if (data.code == 0){
+					if (data.status == "true"){
+						setError(element);
 					}else{
-						$('input[name="confirmPassword"]').next().hide();
-						error["confirmPassword"]={};
-						error["confirmPassword"].isError = false;
-						password = element.val();
-						if (sourceError == "confirmPassword"){
-							admin_messageDiv.hide();
-						}
+						setSuccess(element);
 					}
-					password = element.val();
+				}else{
+					setWarning(element);
 				}
-				break;
-			case "confirmPassword":
-				if(element.val()!=""){
-					var passwordtmp;
-					if ( ( passwordtmp=$('input[name="password"]').val())!= element.val()){
-						error["confirmPassword"].isError = true;
-						element.next().show();
-						showError(CONFIRMPASSWORDNOTCORRECT,"confirmPassword");
-					}else{
-						if (sourceError == attribute){
-							admin_messageDiv.hide();
-						}
-						element.next().hide();
-						error["confirmPassword"]={};
-						error["confirmPassword"].isError = false;
-						password = passwordtmp;
-					}
-				}
-				break;
-			case "role":
-				element.val("");
-				break;
-			default:
-				break;	
+			},"json");
+			username = element.val();
 		}
 	});
 	
-	$('form').submit(function(e){
+	$("#ldapUser").change(function() {
+		if(this.checked) {
+			bLDAPUser = true;
+			$("#passwordSection").hide();
+			$("#confirmPasswordSection").hide();
+			$("#password-input").val('');
+			$("#confirm-password-input").val('');
+			clearStatus($("#password-input"));
+			clearStatus($("#confirm-password-input"));
+		} else {
+			bLDAPUser = false;
+			$("#passwordSection").show();
+			$("#confirmPasswordSection").show();
+		}
+	});
+	
+	$("#confirm-password-input").change(function(e) {
+		checkPasswordOrLdap();
+	});
+	
+	$("#password-input").change(function(e) {
+		checkPasswordOrLdap();
+	});
+	
+	$('#roles-input').autocomplete({
+		source : listRoles,
+		select : function(event,ui){
+			var role = ui.item.value;
+			timeouts.push(setTimeout(function(){
+				$('#roles-input').val("");
+			},500));
+			if (roles.indexOf(role)==-1){
+				var html = $(htmlRole(role));
+				html.find(".delete").click(function(e){
+					var element = $(e.target);
+					while(!element.hasClass("inline_block")){
+						element = element.parent();
+					}
+					element.remove();
+					var index = roles.indexOf(role);
+					if (index > -1) {
+					    roles.splice(index, 1);
+					}
+					checkRoles();
+				});
+				$("#roles").append(html);
+				roles.push(role);
+				checkRoles();
+			}
+		}
+	});
+	
+	function checkUsername() {
+		return $("#username-input").closest(".form-group").hasClass('has-success');
+	}
+	
+	function checkPasswordOrLdap() {
+		let result = $("#ldapUser").is(":checked");
+		clearStatus($("#password-input"));
+		clearStatus($("#confirm-password-input"));
+		if (!result) {
+			result = $("#password-input").val() == $("#confirm-password-input").val() && $("#password-input").val() && $("#password-input").val() != '';
+			if(result) {
+				setSuccess($("#password-input"));
+				setSuccess($("#confirm-password-input"));
+			} else {
+				setError($("#password-input"));
+				setError($("#confirm-password-input"));
+			}
+		}
+		return result;
+	}
+	
+	function checkRoles() {
+		let result = !(roles==null || roles.length==0);
+		clearStatus($('#roles-input'));
+		if (result) {
+			setSuccess($('#roles-input'));
+		} else {
+			setError($('#roles-input'));
+		}
+		return result;
+	}
+	
+	function checkForm() {
+		let result = true;
+		result = checkUsername() && result;
+		result = checkPasswordOrLdap()&& result;
+		result = checkRoles() && result;
+		return result;
+	};
+
+	$('#addForm').submit(function(e){
 		e.preventDefault();
+		if ($("#add-button").hasClass('disabled')) {
+			return;
+		}
+		$("#add-button").button("loading");
+		if (!checkForm()) {
+			$("#add-button").button("reset");
+			return false;
+		}
+		
+		let username = $("#username-input").val();
+		let password = $("#password-input").val();
+		let confirmPassword = $("#confirm-password-input").val();
+		let bLDAPUser = $("#ldapUser").is(":checked");
 		if (username==null || username=="" || (password==null && !bLDAPUser) || (password=="" && !bLDAPUser) || roles==null || roles.length==0){
 			error["all"]={};
 			error["all"].isError = true;
 			showError(FIELDNOTFILLED,"all");
+			$("#add-button").button("reset");
 			return false;
 		}
 		for (var index in error){
 			if (error[index].isError){
 				admin_messageDiv.text(error[index].message).addClass("danger").prepend('<i class="fa fa-exclamation-triangle"></i>').show();
+				$("#add-button").button("reset");
 				return false;
 			}
 		}
-		
-		
+
 		$.post("../SearchAdministrator/addUser",{
 			username : username,
 			password : password,
@@ -247,40 +317,15 @@ $(document).ready(function(){
 					roles = [];
 					username = null;
 					password = null;
-					
-				},1500));		
+					clearStatus($("#username-input"));
+					clearStatus($("#password-input"));
+					clearStatus($('#roles-input'));
+				},1500));
 			}else{
 				showError(data.code,"all");
 			}
+			$("#add-button").button("reset");
 		},"json");
 		return false;
-	});
-	
-	$('.input_roles').autocomplete({
-		source : listRoles,
-		select : function(event,ui){
-			var role = ui.item.value;
-			timeouts.push(setTimeout(function(){
-				$('.input_roles').val("");
-			},500));
-			console.log(role);
-			console.log(roles.indexOf(role));
-			if (roles.indexOf(role)==-1){
-				var html = $(htmlRole(role));
-				html.find(".delete").click(function(e){
-					var element = $(e.target);
-					while(!element.hasClass("inline_block")){
-						element = element.parent();
-					}
-					element.remove();
-					var index = roles.indexOf(role);
-					if (index > -1) {
-					    roles.splice(index, 1);
-					}
-				});
-				$("#roles").append(html);
-				roles.push(role);
-			}
-		}
 	});
 });
