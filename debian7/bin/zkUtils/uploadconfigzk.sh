c92a8018dac432e7f303e590e2e6c00913194330
Romaric Pighetti <romaric.pighetti@francelabs.com>
2017-12-22 17:14:32 +0100

    DATAFARI-227-Do-not-upload-managed-schema-file
    Copy the config to be transferred in a temporary folder and remove the
    managed schema from this copy before the upload to zookeeper.
    The temporary copy is deleted once the upload is done.

--- debian7/bin/zkUtils/uploadconfigzk.sh
+++ debian7/bin/zkUtils/uploadconfigzk.sh
@@ -5,9 +5,9 @@
 #
 #
 
-
-
-"$1/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost $2 -confdir $1/solr/solrcloud/$3/conf/ -confname $3
-
-
-
+tmpFolder=`date +%s`_zk_config;
+mkdir -p $1/.tmp/$tmpfolder;
+cp -R $1/solr/solrcloud/$3/conf/ $1/.tmp/$tmpFolder;
+rm $1/.tmp/$tmpFolder/managed-schema;
+"$1/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost $2 -confdir $1/.tmp/$tmpFolder -confname $3;
+rm -R $1/.tmp/$tmpFolder;
