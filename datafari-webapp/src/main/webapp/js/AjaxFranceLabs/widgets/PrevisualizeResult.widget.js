2324c04f9443bb90ca83fdd7e687c3ec72326ae5
otavard <gitoliv@francelabs.com>
2019-05-15 14:19:00 +0200

    Fix PrevisualizeResultWidget

--- datafari-webapp/src/main/webapp/js/AjaxFranceLabs/widgets/PrevisualizeResult.widget.js
+++ datafari-webapp/src/main/webapp/js/AjaxFranceLabs/widgets/PrevisualizeResult.widget.js
@@ -1,102 +1,88 @@
-AjaxFranceLabs.PrevisualizeResultWidget = AjaxFranceLabs.ResultWidget.extend({
-	elmSelector : '#previsualize',
-	id : 'previ',
-	pagination : false,
-	firstTimeWaypoint : true,
-	isMobile : $(window).width()<800,
-	mutex_locked:false,
+AjaxFranceLabs.PrevisualizeResultWidget = AjaxFranceLabs.AbstractWidget.extend({
+  elmSelector : '#previsualize',
+  id : 'previ',
 
-	buildWidget : function () {
-		this.elm = $(this.elmSelector);
-		this._super();
-	},
+  buildWidget : function() {
+    this.elm = $(this.elmSelector);
+  },
 
-	afterRequest : function() {
-		var data = this.manager.response, elm = $(this.elm),self=this;
+  afterRequest : function() {
+    var data = this.manager.response, elm = $(this.elm), self = this;
 
-		if (!this.isMobile)
-			elm.find('.doc_list').empty();
-		else
-			elm.find('.doc_list .bar-loader').remove();
+    var querySolr = getParamValue('query', decodeURIComponent(window.location.search));
+    var self = this;
+    var docs = self.manager.response.response.docs;
+    var preview_content = "";
+    var title = "";
+    var last_modified = "";
+    var crawl_date = "";
+    var author = "";
+    var file_size = "";
 
-		var querySolr = getParamValue('query', decodeURIComponent(window.location.search));
-		var self = this;
-		var docs = self.manager.response.response.docs;
-		var preview_content = "";
-		var title = "";
-		var last_modified="";
-		var crawl_date="";
-		var author="";
-		var file_size="";
+    var dummy1 = "Dummy text 1";
+    var dummy2 = "Dummy text 2";
+    var dummy3 = "Dummy text 3";
+    var dummy4 = "Dummy text 4";
 
-		var dummy1= "Dummy text 1";
-		var dummy2= "Dummy text 2";
-		var dummy3= "Dummy text 3";
-		var dummy4= "Dummy text 4";
+    if (docs.length != 0) {
+      $.each(docs, function(index, doc) {
+        if (docs[index].preview_content != undefined) {
+          preview_content = docs[index].preview_content;
+        }
 
+        if (docs[index].title != undefined) {
+          title = docs[index].title;
+        }
 
+        if (docs[index].last_modified != undefined) {
+          last_modified = formatDate(docs[index].last_modified,"en-US");
+        }
 
-		if (docs.length!=0){
-			$.each(docs,function(index,doc){
-				if (docs[index].preview_content != undefined){
-					preview_content = docs[index].preview_content;
-				}
+        if (docs[index].crawl_date != undefined) {
+          crawl_date = formatDate(docs[index].crawl_date,"en-US");
+        }
 
-				if (docs[index].title != undefined){
-					title = docs[index].title;
-				}
+        if (docs[index].author != undefined) {
+          author = docs[index].author;
+        }
 
-				if (docs[index].last_modified != undefined){
-					last_modified = docs[index].last_modified;
-				}
+        if (docs[index].original_file_size != undefined) {
+          file_size = docs[index].original_file_size;
+          file_size = nFormatter(file_size,1);
+        }
 
-				if (docs[index].crawl_date != undefined){
-					crawl_date = docs[index].crawl_date;
-				}
+        // Add here the information that you want to add in the previsualize
+        // window
+        var htmlContent = '<table border="0" style="margin:10px">';
+        htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Last Modified</b></td><td><b style="font-weight: bold;">Crawl date:</b></td></tr>';
+        htmlContent += '<tr><td style="width:170px">' + last_modified + '</td><td>' + crawl_date + '</td></tr>';
+        htmlContent += '<tr><td></td><td></td></tr>';
+        htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Author:</b></td><td><b style="font-weight: bold;">File Size :</b></td></tr>';
+        htmlContent += '<tr><td style="width:170px">' + author + '</td><td>' + file_size + 'B</td></tr>';
+        htmlContent += '<tr><td></td><td></td></tr>';
+        /*
+        htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Dummy1:</b></td><td><b style="font-weight: bold;">Dummy2:</b></td></tr>';
+        htmlContent += '<tr><td style="width:170px">' + dummy1 + '</td><td>' + dummy2 + '</td></tr>';
+        htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Dummy3:</b></td><td><b style="font-weight: bold;">Dummy4:</b></td></tr>';
+        htmlContent += '<tr><td style="width:170px">' + dummy3 + '</td><td>' + dummy4 + '</td></tr>';
+        */
+        htmlContent += '</table>';
+        htmlContent += '<textarea style="width:250px;height:200px;border-color: #ccc;">' + preview_content + '</textarea>';
+        /*
+        htmlContent += '<div style="padding: 10px;background-color: #ccc;color: #000;">';
+        htmlContent += '<span style="margin-right:15px; margin-left:10px"><b style="font-weight: bold;"> SHARE </b></span>';
+        htmlContent += '<span><b style="font-weight: bold;"> MORE LIKE THIS </b></span></div>';
+        */
+        $($('.doc_list .res')[index]).append('<div class="previsualizetemplate arrow_box" style="width:320px;box-shadow: #ccc 0px 5px 5px 1px;">');
+        $($('.doc_list .previsualizetemplate')[index]).append('<div class="inner-side-content">');
+        $($('.doc_list .inner-side-content')[index]).append('<div style="margin:10px;font-size:17px"><b>' + title + '</b></div>');
+        $($('.doc_list .inner-side-content')[index]).append(htmlContent);
+        $($('.doc_list .previsualizetemplate')[index]).append('</div>');
+        $($('.doc_list .res')[index]).append('</div>');
 
-				if (docs[index].author != undefined){
-					author = docs[index].author;
-				}
+      });
 
-				if (docs[index].file_size != undefined){
-					author = docs[index].file_size;
-				}
+    }
 
-
-				// Add here the information that you want to add in the previsualize window
-				var htmlContent = '<table border="0" style="margin:10px">';
-				htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Last Modified</b></td><td><b style="font-weight: bold;">Crawl date:</b></td></tr>';			
-				htmlContent += '<tr><td style="width:170px">'+last_modified+'</td><td>'+crawl_date+'</td></tr>';			
-				htmlContent += '<tr><td></td><td></td></tr>';			
-				htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Author:</b></td><td><b style="font-weight: bold;">File Size :</b></td></tr>';			
-				htmlContent += '<tr><td style="width:170px">'+author+'</td><td>'+file_size+'</td></tr>';			
-				htmlContent += '<tr><td></td><td></td></tr>';			
-				htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Dummy1:</b></td><td><b style="font-weight: bold;">Dummy2:</b></td></tr>';			
-				htmlContent += '<tr><td style="width:170px">'+dummy1+'</td><td>'+dummy2+'</td></tr>';			
-				htmlContent += '<tr><td style="width:170px"><b style="font-weight: bold;">Dummy3:</b></td><td><b style="font-weight: bold;">Dummy4:</b></td></tr>';			
-				htmlContent += '<tr><td style="width:170px">'+dummy3+'</td><td>'+dummy4+'</td></tr>';			
-				htmlContent += '</table>';			
-				htmlContent += '<textarea style="width:250px;height:200px;border-color: #ccc;">'+preview_content+'</textarea>';			
-				htmlContent += '<div style="padding: 10px;background-color: #ccc;color: #000;">';			
-				htmlContent += '<span style="margin-right:15px; margin-left:10px"><b style="font-weight: bold;"> SHARE </b></span>';			
-				htmlContent += '<span><b style="font-weight: bold;"> MORE LIKE THIS </b></span></div>';			
-
-				$($('.doc_list .res')[index]).append('<div class="previsualizetemplate arrow_box" style="width:320px;box-shadow: #ccc 0px 5px 5px 1px;">');
-				$($('.doc_list .previsualizetemplate')[index]).append('<div class="inner-side-content">');
-				$($('.doc_list .inner-side-content')[index]).append('<div style="margin:10px;font-size:17px"><b>'+title+'</b></div>');
-				$($('.doc_list .inner-side-content')[index]).append(htmlContent);
-				$($('.doc_list .previsualizetemplate')[index]).append('</div>' );
-				$($('.doc_list .res')[index]).append('</div>' );
-
-
-
-			});
-
-
-		}
-
-
-
-
-	}
+  }
 });
