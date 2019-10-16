3d0e9812d92bd2a7ced27f81fe3326dc5e8f88a7
jmassiera <gitjulien@francelabs.com>
2019-10-16 16:49:55 +0200

    Bugfix preview share links # Conflicts: #	datafari-webapp/src/main/webapp/js/AjaxFranceLabs/preview/widgets/PreviewRequestWidget.js

--- datafari-webapp/src/main/webapp/js/AjaxFranceLabs/preview/widgets/ShareLinks-Widget.js
+++ datafari-webapp/src/main/webapp/js/AjaxFranceLabs/preview/widgets/ShareLinks-Widget.js
@@ -1,18 +1,15 @@
-/*******************************************************************************
+/*******************************************************************************************************************************************
  * Copyright 2015 France Labs
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ ******************************************************************************************************************************************/
 AjaxFranceLabs.ShareLinksWidget = AjaxFranceLabs.FacetCore.extend({
 
   // Variables
@@ -30,9 +27,6 @@ AjaxFranceLabs.ShareLinksWidget = AjaxFranceLabs.FacetCore.extend({
       var doc = data.response.docs[0];
       var idDoc = doc.id;
       var idDocLink = idDoc;
-      if (idDoc.startsWith("http")) {
-        idDocLink = encodeURIComponent(idDoc);
-      }
       var sharePreviewLink = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/Datafari/Preview?docId=" + idDocLink
       var sharePreview = $('<div class="share-link-div"><i class="fas fa-chevron-right"></i> ' + window.i18n.msgStore['preview-share-preview'] + ' <br/><input type="text" value="' + sharePreviewLink
           + '" /></div>');
