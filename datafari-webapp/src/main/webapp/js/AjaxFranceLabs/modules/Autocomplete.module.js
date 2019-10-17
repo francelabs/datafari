dba141f88749ab5190703bfa401bcf66e45c0222
jmassiera <gitjulien@francelabs.com>
2019-10-17 15:00:21 +0200

    Fix DATAFARI-405

--- datafari-webapp/src/main/webapp/js/AjaxFranceLabs/modules/Autocomplete.module.js
+++ datafari-webapp/src/main/webapp/js/AjaxFranceLabs/modules/Autocomplete.module.js
@@ -21,169 +21,164 @@
  * @documentation http://www.francelabs.com/en/AjaxFranceLabs/widget-module-documentation#autocomplete_module
  * 
  */
-AjaxFranceLabs.AutocompleteModule = AjaxFranceLabs.AbstractModule
-.extend({
+AjaxFranceLabs.AutocompleteModule = AjaxFranceLabs.AbstractModule.extend({
 
-	// Variables
+  // Variables
 
-	optionsExtend : true,
+  optionsExtend : true,
 
-	options : {},
+  options : {},
 
-	render : null,
+  render : null,
 
-	field : null,
+  field : null,
 
-	openOnFieldFocus : false,
+  openOnFieldFocus : false,
 
-	valueSelectFormat : function(value) {
-		return value
-	},
+  valueSelectFormat : function(value) {
+    return value
+  },
 
-	singleValue : false,
+  singleValue : false,
 
-	type : 'autocomplete',
+  type : 'autocomplete',
 
-	cache : [],
+  cache : [],
 
-	lastXhr : null,
+  lastXhr : null,
 
-	// Methods
+  // Methods
 
-	init : function() {
-		if (!this.initialized) {
-			this.initialized = true;
-			var self = this;
-			var opt = {
-					delay : 50,
-					minLength : 0,
-					source : function(request, response) {
-						var term = AjaxFranceLabs.extractLast(request.term).toLowerCase();
-						if (term in self.cache) {
-							response(self.cache[term]);
-							return;
-						}
-						var f = (self.field === null) ? '' : '&f='
-							+ self.field;
-						// Commented code that encapsulate the query with '"' if white spaces are detected. Assumed to be useless !
-//						if ((AjaxFranceLabs.extractLast(request.term) != '') && ((/\s/.test(AjaxFranceLabs.extractLast(request.term)))))  {
-//							self.lastXhr = self.manager
-//							.executeRequest(
-//									self.connectionInfo.serverUrl,
-//									self.connectionInfo.servlet,
-//									self.connectionInfo.queryString
-//									+ encodeURIComponent('"' + AjaxFranceLabs
-//											.extractLast(request.term)
-//											+ '"')
-//											+ f + "&autocomplete=true",
-//											function(data, status, xhr) {
-//										var src = [];
-//										// self.manager.executeRequest('',
-//										// '', 'q=' +
-//										// AjaxFranceLabs.extractLast(request.term)
-//										// + f,
-//										// function(data) {
-//										if (data.spellcheck.suggestions.length > 1) {
-//
-//											$.each(data.spellcheck.collations, function(index, value) {
-//												if (value != 'collation'){
-//													src.push(data.spellcheck.collations[index].collationQuery.replace(/"/g, ''));
-//												}
-//
-//											});
-//
-//											self.cache[term] = src;
-//											if (xhr === self.lastXhr) {
-//												response(src);
-//											}
-//										}
-//
-//									});
-//							// });
-//						}
-//						else 
-							
-						if (AjaxFranceLabs.extractLast(request.term) != '') {
-							self.lastXhr = self.manager
-							.executeRequest(
-									self.connectionInfo.serverUrl,
-									self.connectionInfo.servlet,
-									self.connectionInfo.queryString
-									+ encodeURIComponent('' + AjaxFranceLabs
-											.extractLast(request.term)
-											+ '')
-											+ f + "&autocomplete=true" + "&spellcheck.collateParam.q.op=" + self.manager.store.get("q.op").val(),
-											function(data, status, xhr) {
-										var src = [];
-										// self.manager.executeRequest('',
-										// '', 'q=' +
-										// AjaxFranceLabs.extractLast(request.term)
-										// + f,
-										// function(data) {
-										if (data.spellcheck.collations.length > 1) {
+  init : function() {
+    if (!this.initialized) {
+      this.initialized = true;
+      var self = this;
+      var opt = {
+        delay : 50,
+        minLength : 0,
+        source : function(request, response) {
 
-											$.each(data.spellcheck.collations, function(index, value) {
-												if (value != 'collation'){
-													src.push(data.spellcheck.collations[index].collationQuery.replace(/"/g, ''));
-												}
+          // Abort any previously triggered request
+          if (self.lastXhr != null) {
+            self.lastXhr.abort();
+          }
 
-											});
+          var term = AjaxFranceLabs.extractLast(request.term).toLowerCase();
+          if (term in self.cache) {
+            response(self.cache[term]);
+            return;
+          }
+          var f = (self.field === null) ? '' : '&f=' + self.field;
+          // Commented code that encapsulate the query with '"' if white spaces are detected. Assumed to be useless !
+          // if ((AjaxFranceLabs.extractLast(request.term) != '') && ((/\s/.test(AjaxFranceLabs.extractLast(request.term))))) {
+          // self.lastXhr = self.manager
+          // .executeRequest(
+          // self.connectionInfo.serverUrl,
+          // self.connectionInfo.servlet,
+          // self.connectionInfo.queryString
+          // + encodeURIComponent('"' + AjaxFranceLabs
+          // .extractLast(request.term)
+          // + '"')
+          // + f + "&autocomplete=true",
+          // function(data, status, xhr) {
+          // var src = [];
+          // // self.manager.executeRequest('',
+          // // '', 'q=' +
+          // // AjaxFranceLabs.extractLast(request.term)
+          // // + f,
+          // // function(data) {
+          // if (data.spellcheck.suggestions.length > 1) {
+          //
+          // $.each(data.spellcheck.collations, function(index, value) {
+          // if (value != 'collation'){
+          // src.push(data.spellcheck.collations[index].collationQuery.replace(/"/g, ''));
+          // }
+          //
+          // });
+          //
+          // self.cache[term] = src;
+          // if (xhr === self.lastXhr) {
+          // response(src);
+          // }
+          // }
+          //
+          // });
+          // // });
+          // }
+          // else
 
-											self.cache[term] = src;
-											if (xhr === self.lastXhr) {
-												response(src);
-											}
-										}
+          if (AjaxFranceLabs.extractLast(request.term) != '') {
+            self.lastXhr = self.manager.executeRequest(self.connectionInfo.serverUrl, self.connectionInfo.servlet, self.connectionInfo.queryString
+                + encodeURIComponent('' + AjaxFranceLabs.extractLast(request.term) + '') + f + "&autocomplete=true" + "&spellcheck.collateParam.q.op=" + self.manager.store.get("q.op").val(),
+                function(data, status, xhr) {
+                  var src = [];
+                  // self.manager.executeRequest('',
+                  // '', 'q=' +
+                  // AjaxFranceLabs.extractLast(request.term)
+                  // + f,
+                  // function(data) {
+                  if (data.spellcheck.collations.length > 1) {
 
-									}, function(){}, 30000);
-							// });
-						}
-					},
-					search : function() {
-						var term = AjaxFranceLabs.extractLast(this.value);
-						if (term.length < self.autocompleMinLength) {
-							return false;
-						}
-					},
-					select : function(event, ui) {
-						if (self.singleValue === true) {
-							this.value = self
-							.valueSelectFormat(ui.item.value);
-							this.value +=  ("\u200c");
-						} else {
-							var terms = AjaxFranceLabs.split(this.value);
-							terms.pop();
-							terms.push(self
-									.valueSelectFormat(ui.item.value));
-							terms.push("");
-							this.value = terms.join("\u200c ");
-							this.value = this.value.slice(0,-2);
-						}
-						return false;
-					},
-					focus : function() {
-						return false;
-					}
-			};
-			if (this.optionsExtend === true) {
-				var newOpt = {};
-				$.extend(true, newOpt, opt, this.options);
-				this.options = newOpt;
-			}
-			$(this.elm).autocomplete(this.options);
-			if (this.openOnFieldFocus === true) {
-				$(this.elm).click(function() {
-					$(this).autocomplete('search');
-				});
-			}
-			if (typeof this.render === 'function') {
-				$(this.elm).data("autocomplete")._renderItem = this.render;
-			}
-			if (this.manager.connectionInfo.autocomplete === undefined
-					&& this.options.source == null)
-				throw 'AutocompleteModule: connectionInfo not defined in Manager';
-			else
-				this.connectionInfo = this.manager.connectionInfo.autocomplete;
-		}
-	}
+                    $.each(data.spellcheck.collations, function(index, value) {
+                      if (value != 'collation') {
+                        src.push(data.spellcheck.collations[index].collationQuery.replace(/"/g, ''));
+                      }
+
+                    });
+
+                    self.cache[term] = src;
+                    if (xhr === self.lastXhr) {
+                      response(src);
+                    }
+                  }
+
+                }, function() {
+                }, 30000);
+            // });
+          }
+        },
+        search : function() {
+          var term = AjaxFranceLabs.extractLast(this.value);
+          if (term.length < self.autocompleMinLength) {
+            return false;
+          }
+        },
+        select : function(event, ui) {
+          if (self.singleValue === true) {
+            this.value = self.valueSelectFormat(ui.item.value);
+            this.value += ("\u200c");
+          } else {
+            var terms = AjaxFranceLabs.split(this.value);
+            terms.pop();
+            terms.push(self.valueSelectFormat(ui.item.value));
+            terms.push("");
+            this.value = terms.join("\u200c ");
+            this.value = this.value.slice(0, -2);
+          }
+          return false;
+        },
+        focus : function() {
+          return false;
+        }
+      };
+      if (this.optionsExtend === true) {
+        var newOpt = {};
+        $.extend(true, newOpt, opt, this.options);
+        this.options = newOpt;
+      }
+      $(this.elm).autocomplete(this.options);
+      if (this.openOnFieldFocus === true) {
+        $(this.elm).click(function() {
+          $(this).autocomplete('search');
+        });
+      }
+      if (typeof this.render === 'function') {
+        $(this.elm).data("autocomplete")._renderItem = this.render;
+      }
+      if (this.manager.connectionInfo.autocomplete === undefined && this.options.source == null)
+        throw 'AutocompleteModule: connectionInfo not defined in Manager';
+      else
+        this.connectionInfo = this.manager.connectionInfo.autocomplete;
+    }
+  }
 });
