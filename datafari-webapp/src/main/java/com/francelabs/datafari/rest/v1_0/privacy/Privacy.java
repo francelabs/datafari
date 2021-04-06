bffe6259e6fa1b3e94495072283e233abb795d27
Romaric Pighetti <romaric.pighetti@francelabs.com>
2021-04-06 15:57:42 +0200

    Adding API endpoint to retrieve privacy policy page

--- datafari-webapp/src/main/java/com/francelabs/datafari/rest/v1_0/privacy/Privacy.java
+++ datafari-webapp/src/main/java/com/francelabs/datafari/rest/v1_0/privacy/Privacy.java
@@ -15,35 +15,84 @@
  *******************************************************************************/
 package com.francelabs.datafari.rest.v1_0.privacy;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import com.francelabs.datafari.rest.v1_0.exceptions.InternalErrorException;
+import com.francelabs.datafari.rest.v1_0.utils.RestAPIUtils;
+import com.francelabs.datafari.utils.Environment;
+import com.francelabs.datafari.utils.ExecutionEnvironment;
 
 import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@WebServlet("/rest/v1.0/privacy")
-public class Privacy extends HttpServlet {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RestController
+public class Privacy {
+
+    private static final String BASE_FILE_NAME = "privacyPolicyContent";
+    private static final String FILE_EXTENSION = ".jsp";
+    private final static Logger LOGGER = LogManager.getLogger(Privacy.class.getName());
+
+    @GetMapping(value = "/rest/v1.0/privacy/{lang}", produces = "application/json;charset=UTF-8")
+    protected String getCurrentUserAlerts(final HttpServletRequest request, @PathVariable("lang") String lang) {
+        final JSONObject jsonResponse = new JSONObject();
+        String environnement = Environment.getEnvironmentVariable("DATAFARI_HOME");
+
+        if (environnement == null) { // If in development environment
+            environnement = ExecutionEnvironment.getDevExecutionEnvironment();
+        }
+        String basePath = environnement + File.separator + "tomcat" + File.separator + "webapps" + File.separator
+                + "Datafari" + File.separator + "resources" + File.separator + "privacyPolicyAssets";
+        
+        try {
+            String filepath = basePath + File.separator + BASE_FILE_NAME + "_" + lang + FILE_EXTENSION;
+            String fileContent = readFile(filepath, StandardCharsets.UTF_8);
+            // Get rid of the first line which is charset definition
+            fileContent = fileContent.substring(fileContent.indexOf('\n') + 1);
+            jsonResponse.put("htmlPrivacyContent", fileContent);
+            return RestAPIUtils.buildOKResponse(jsonResponse);
+        } catch (NoSuchFileException e) {
+            try {
+                // Language specific help file not found, try the generic one
+                final String defaultFilePath = basePath + File.separator + BASE_FILE_NAME + FILE_EXTENSION;
+                String fileContent;
+                fileContent = readFile(defaultFilePath, StandardCharsets.UTF_8);
+                // Get rid of the first line which is charset definition
+                fileContent = fileContent.substring(fileContent.indexOf('\n') + 1);
+                jsonResponse.put("htmlPrivacyContent", fileContent);
+                return RestAPIUtils.buildOKResponse(jsonResponse);
+            } catch (IOException e1) {
+                LOGGER.warn("Unexpected error while retrieving privacy policy content with rest API", e1);
+                throw new InternalErrorException("Unexpected error while retrieving privacy policy content");
+            }
+        } catch (IOException e) {
+            LOGGER.warn("Unexpected error while retrieving privacy policy content with rest API", e);
+            throw new InternalErrorException("Unexpected error while retrieving privacy policy content");
+        }
+    }
 
     /**
-     * Automatically generated serial ID
+     * Reads the file from the fileSystem and output its content as a String.
+     *
+     * @param path     Path of the file to be read
+     * @param encoding Encoding of the file
+     * @return The content of the file as a String
+     * @throws IOException
      */
-    private static final long serialVersionUID = -7963179533577734482L;
-
-    @Override
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) {
-        try {
-            response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
-            final JSONObject jsonResponse = new JSONObject();
-            jsonResponse.put("error", "Not Implemented");
-            PrintWriter out;
-            out = response.getWriter();
-            out.print(jsonResponse);
-        } catch (IOException e) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
+    private static String readFile(final String path, final Charset encoding) throws IOException {
+        final byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 }
--- datafari-webapp/src/main/webapp/resources/privacyPolicyAssets/privacyPolicyContent.jsp
+++ datafari-webapp/src/main/webapp/resources/privacyPolicyAssets/privacyPolicyContent.jsp
@@ -1,18 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=utf-8"
-    pageEncoding="utf-8"%>
-<h1>Politique de Confidentialité</h1>
+<h1>Privacy Policy</h1>
 <p>
-    DATAFARI est un moteur de recherche permettant la gestion de contenu (exploration, indexation et
-    recherche sur des données). Les profils de ses utilisateurs ne sont ni traités ni analysés.
-    Ils sont utilisés pour améliorer l’expérience utilisateur en supprimant le besoin de déclarer ces
-    valeurs à chaque connexion de l’utilisateur. Par défaut, chaque utilisateur est maitre du contrôle
-    de ses données : il peut accéder et modifier ses données de préférence via une interface
-    personnelle. Les données des logs sont conservées pour une durée fixée par votre organisation 
-    à des fins de sécurité et ne sont accessibles qu’aux agents en charge de la sécurité du système.
-</p>
-
+    DATAFARI is a search engine allowing content management (exploration, indexations and search
+    on the content). User profiles are not analysed nor processed. They are used to provide a better
+    user experience by removing the need to redefine any preference at each new connection to the
+    tool. By default, each user is able to control the data about his account: he can access and
+    modify his account preferences through a personal interface. Log data are kept for a period
+    defined by your organization for security reasons and are accessible only to the people
+    responsible for that security.</p>
 <p>
-    Pour toutes questions relatives à l’usage de vos droits d’accès, de rectification et d’effacement
-    sur vos données à caractère personnel vous pouvez contacter le correspondant RGPD de votre
-    organisation à l’adresse suivante : <a href="mailto:"></a>
+    For any question related to your rights to access, modify or delete your personal data, you can
+    contact you organization GDPR correspondant at the following address: <a href="mailto:"></a>
 </p>
