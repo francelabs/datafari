5c26b0c833159e8613fd14da5d2cfc4f0f2769f8
Julien Massiera <julien.massiera@francelabs.com>
2022-05-24 17:56:15 +0200

    Fixes #831: Update Logstash OSS to v8.2.0

--- datafari-elk/scripts/set-elk-env.sh
+++ datafari-elk/scripts/set-elk-env.sh
@@ -12,6 +12,7 @@ export ELK_HOME=$DATAFARI_HOME/elk
 export ELK_LOGS=$DATAFARI_HOME/logs/elk
 export LOGSTASH_HOME=$ELK_HOME/logstash
 export TIKA_SERVER_HOME=$DATAFARI_HOME/tika-server
+export LS_JAVA_HOME=$JAVA_HOME
 
 # pid files 
 export LOGSTASH_PID_FILE=$PID_DIR/logstash.pid
