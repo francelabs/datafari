7699d39d8a7de2c7edfee039c4a825c468963933
guylaine-bassette <guylaine.bassette@francelabs.com>
2024-01-08 15:13:02 +0100

    JAR approach: Use the SolrJ Solr Cloud Client to connect and send request to Solr.

--- datafari-solr-atomic-update/src/main/java/com/francelabs/datafari/solraccessors/AbstractDocuments.java
+++ datafari-solr-atomic-update/src/main/java/com/francelabs/datafari/solraccessors/AbstractDocuments.java
@@ -3,10 +3,12 @@ package com.francelabs.datafari.solraccessors;
 import com.francelabs.datafari.config.CollectionPathConfig;
 import com.francelabs.datafari.config.JobConfig;
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.impl.Http2SolrClient;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public abstract class AbstractDocuments implements AutoCloseable{
   protected SolrClient solrClient;
@@ -30,18 +32,20 @@ public abstract class AbstractDocuments implements AutoCloseable{
 
   protected abstract CollectionPathConfig getCollectionPath();
 
-  protected SolrClient getSolrClient(final String baseSolrUrl){
-    //TODO best for SolrCloud mode
-    /*final List<String> zkServers = new ArrayList<>();
-    //zkServers.add("zookeeper1:2181");
-    //zkServers.add("zookeeper2:2181");
-    zkServers.add("https://dev.datafari.com:2181"); // ?
-    solrClient = new CloudSolrClient.Builder(zkServers, Optional.of("/solr")).build();*/
-    return new Http2SolrClient.Builder(baseSolrUrl)
-        .withConnectionTimeout(10000, TimeUnit.MILLISECONDS)
-        .withIdleTimeout(60000, TimeUnit.MILLISECONDS)
-        .build();
+  protected SolrClient getSolrClient(final String solrBaseUrls){
+    String[] tabBaseSolUrls = solrBaseUrls.split(",");
+    final List<String> cloudServers = new ArrayList<>();
+    for (String baseSolrUrl : tabBaseSolUrls){
+      cloudServers.add(baseSolrUrl.trim());
+    }
+    SolrClient client;
+    if (tabBaseSolUrls[0].startsWith("http")){ // Solr server(s)
+      client = new CloudSolrClient.Builder(cloudServers).build();
+    } else { // Zookeeper server(s)
+      client = new CloudSolrClient.Builder(cloudServers, Optional.empty()).build();
+    }
 
+    return client;
   }
   @Override
   public void close() throws Exception {
