2271c31b1f8531268188e629598b1063e68b169e
otavard <gitoliv@francelabs.com>
2024-03-20 09:59:52 +0100

    Fix Purge analytics log not working into multiservers mode for Enterprise Edition

--- linux/bin/init-datafari.sh
+++ linux/bin/init-datafari.sh
@@ -241,6 +241,7 @@ init_solr_node() {
   sed -i -e "s/@SOLRNODEIP@/${1}/g" $TOMCAT_HOME/conf/solr.properties >>$installerLog 2>&1
   sed -i -e "s/@SOLRNODEIP@/${1}/g" $DATAFARI_HOME/ssl-keystore/apache/config/tomcat.conf >>$installerLog 2>&1
   sed -i -e "s/@SOLRNODEIP@/${1}/g" $SOLR_INSTALL_DIR/solr_home/FileShare/conf/customs_schema/addCustomSchemaInfo.sh >>$installerLog 2>&1
+  sed -i -e "s/@SOLRNODEIP@/${1}/g" $DATAFARI_HOME/bin/purgeUtils/analyticsPurge.sh >>$installerLog 2>&1
     
 }
 
