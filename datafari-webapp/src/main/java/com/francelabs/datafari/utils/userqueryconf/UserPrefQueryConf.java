6dff594958846fe92c62c595a66872a2a7548ee7
guylaine-bassette <guylaine.bassette@francelabs.com>
2024-03-26 14:37:04 +0100

    CE#813: change Query boost parameter "bq" for "boost".

--- datafari-webapp/src/main/java/com/francelabs/datafari/utils/userqueryconf/UserPrefQueryConf.java
+++ datafari-webapp/src/main/java/com/francelabs/datafari/utils/userqueryconf/UserPrefQueryConf.java
@@ -4,7 +4,7 @@ import com.francelabs.datafari.exception.DatafariServerException;
 import com.francelabs.datafari.service.db.UiConfigDataService;
 import com.francelabs.datafari.utils.SolrAPI;
 import com.sun.istack.NotNull;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
@@ -64,7 +64,7 @@ public class UserPrefQueryConf extends AUserQueryConf {
 
   @Override
   protected String[] getQueryConfKeys() {
-    return new String[]{SolrAPI.QUERY_BQ};
+    return new String[]{SolrAPI.QUERY_BOOST};
   }
 
   /**
@@ -92,7 +92,8 @@ public class UserPrefQueryConf extends AUserQueryConf {
 
   /**
    * Build the Solr query parameter related to the user preferences (chosen in Datafari's UI).
-   * Create a "bq" query parameter. Example: "bq":"repo_source:(source_3^1 OR source_2^2 OR source_1^3) langue:(fr^2 OR en^1)"
+   * Create a "boost" query parameter. Example: "boost":"product( product( if( eq(repo_source, 'source2'),300,1 ), if( eq(repo_source, 'source3'),200,1 )), if( eq(repo_source, 'enron'),100,1 )"
+   * Apply product operation as many times as necessary to combine conditions on fields.
    *
    * @param username
    * @return the json formatted Solr query parameter for the given user. Return an empty string if no configuration found
@@ -113,12 +114,14 @@ public class UserPrefQueryConf extends AUserQueryConf {
       StringBuilder queryFieldPart;
       for(Map.Entry<String, String> userPrefMapEntry: USER_QUERY_PREF_MAPPER.entrySet()) {
         preferenceValues = (JSONArray) jsonUiConfig.get(userPrefMapEntry.getKey());
-        if (preferenceValues != null && preferenceValues.size()>0){
+        // Must have more than 1 preference value to require boosting.
+        if (preferenceValues != null && preferenceValues.size()>1){
           solrField = userPrefMapEntry.getValue();
 
-          // Compute query parameter for this solrField: field:(value1^weight1 OR value2^weight2 OR...)
+          // Compute query parameter for this solrField: product( product( if( eq(field,value1), weight1,1), if( eq(field,value2), weight2, 1) ) ), if(...) )
           queryFieldPart = computeQueryBoostParameterField(solrField, preferenceValues);
-          result.append(queryFieldPart).append(" ");
+          // Wrap into "product" function when 2 values are available.
+          result = wrapProduct(result, queryFieldPart);
         }
       }
     } catch (ParseException e) {
@@ -127,34 +130,69 @@ public class UserPrefQueryConf extends AUserQueryConf {
     }
 
     if (StringUtils.isNotBlank(result.toString())){
-      // Complete configuration syntax: {"bq":"field:(value1^weight1...)"}
-      result.insert(0, "\""+ SolrAPI.QUERY_BQ +"\":\"");
+      // Complete configuration syntax:
+      //   - result comes with functions_conditions.
+      //   - we must wrap functions_conditions with: "boost":"functions_conditions"
+      result.insert(0, "\""+ SolrAPI.QUERY_BOOST +"\":\"");
       result.append("\"");
     }
     return result.toString();
   }
 
   /**
-   * Creates one "bq" field parameter, for example: repo_source:(source_3^1 OR source_2^2 OR source_1^3)
-   * Conforms to Solr syntax for "bq" Boost Query parameter.
+   * Creates one "boost" query field parameter, with "if" function to give a weight to the value of the field, and "product" function to wrap "if" functions and "product"
+   * to combine like this : product( product( if( eq(repo_source, 'source2'),30,1 ), if( eq(repo_source, 'source3'),20,1 )), if( eq(repo_source, 'enron'),10,1 )
    *
    * @param solrField the solr field to compute the configuration for. In the example above solrField=repo_source
    * @param orderedValues the field values ordered to have the biggest weight first and then decrease. In the example above
-   *                      orderedValues=[source_3, source_2, source_1]. The weights 3, 2 and 1 will be assigned.
-   * @return the field configuration for the "bq" parameter.
+   *                      orderedValues=[source_3, source_2, source_1]. The weights 3*X, 2*X and 1*X will be assigned.
+   * @return a nested sequence of IFs and Products for the given solrField. In another word, combination of "product" and "if" functions for a field and its values/weight.
    */
   private StringBuilder computeQueryBoostParameterField(String solrField, JSONArray orderedValues){
-    StringBuilder result = new StringBuilder(solrField).append(":(");
+    StringBuilder result = new StringBuilder();
     int nbValues = orderedValues.size();
+    StringBuilder ifFunction;
     for (Object value : orderedValues) {
-      result.append(value).append("^").append(nbValues);
+      // create "if" function
+      ifFunction = createIF(solrField, value.toString(), nbValues);
+      // Wrap into "product" function when 2 values are available.
+      result = wrapProduct(result, ifFunction);
+
       nbValues--;
-      if (nbValues > 0){
-        result.append(" OR ");
-      }
     }
-    result.append(")");
     return result;
+  }
+
+  /**
+   * Create the "if" function
+   *
+   * @param solrField
+   * @param value
+   * @param initialWeight
+   * @return
+   */
+  private StringBuilder createIF(String solrField, String value, int initialWeight){
+    StringBuilder ifFunction = new StringBuilder("if(eq(");
+    ifFunction.append(solrField).append(",'").append(value).append("'),").append(initialWeight*10).append(",1)");
+    return ifFunction;
+  }
+
+  /**
+   * Wrap into a "product" function the 2 given values. Resulting in a multiplication between first value and second value in a query boost.
+   *
+   * @param firstValue
+   * @param secondValue
+   * @return the String "product(firstValue, secondValue)" if the 2 values are filled. Otherwise, returns the second value if filled.
+   * Otherwise, returns the firstValue.
+   */
+  private StringBuilder wrapProduct(StringBuilder firstValue, StringBuilder secondValue){
+    if (StringUtils.isNotBlank(firstValue) && StringUtils.isNotBlank(secondValue)) {
+      firstValue.insert(0, "product( ").append(", ").append(secondValue).append(" )");
+    } else if (StringUtils.isNotBlank(secondValue)) {
+      firstValue = secondValue;
+    }
+
+    return firstValue;
   }
 
 }
