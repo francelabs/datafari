8c8b602d7f6bfa1e6f20772b934854826cd98fea
otavard <gitoliv@francelabs.com>
2024-04-17 23:38:00 +0200

    Fix datafari-community/datafari#978 Check Solr autocommit maxtime set by Solr API

--- linux/bin/datafari-manager.sh
+++ linux/bin/datafari-manager.sh
@@ -279,12 +279,12 @@ init_solr()
   curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"duplicates.hash.fields": "content"}}' http://localhost:8983/solr/Duplicates/config
   curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"duplicates.quant.rate": "0.1"}}' http://localhost:8983/solr/Duplicates/config 
  
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/@MAINCOLLECTION@/config
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Statistics/config
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Promolink/config
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Access/config
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Monitoring/config
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Duplicates/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/@MAINCOLLECTION@/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Statistics/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Promolink/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Access/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Monitoring/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/Duplicates/config
  
   cd ${SOLR_INSTALL_DIR}/solrcloud/FileShare/conf/customs_schema && bash addCustomSchemaInfo.sh
   @SOLR-INIT@
@@ -294,7 +294,7 @@ init_solr_annotators() {
   curl -XGET "http://localhost:8983/solr/admin/collections?action=CREATE&name=OCR&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
   curl -XGET "http://localhost:8983/solr/admin/collections?action=CREATE&name=Spacy&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
   curl -XGET "http://localhost:8983/solr/admin/collections?action=CREATE&name=GenericAnnotator&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
-  curl -XPOST -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/GenericAnnotator/config
+  curl -XPOST -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "60000"}}' http://localhost:8983/solr/GenericAnnotator/config
  
 }
 
--- linux/bin/zkUtils/init-solr-collections.sh
+++ linux/bin/zkUtils/init-solr-collections.sh
@@ -34,7 +34,8 @@ create_collection() {
   	curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"texttagger.enabled": "false"}}' $url_protocol://${ip_solr}/solr/$1/config
     curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"texttagger.host": "localhost:2181"}}' $url_protocol://${ip_solr}/solr/$1/config
   	curl -XPOST --insecure $url_protocol://${ip_solr}/solr/$1/config/params -H 'Content-type:application/json'  -d '{"set":{"mySearch":{"qf":"exactContent^500 exactTitle^500 title_fr^50 title_en^50 title_de^50 title_es^50 content_fr^10 content_en^10 content_de^50 content_es^50 source^20 id^3 url_search^3","pf":"exactContent^500 exactTitle^500 title_en^500 title_fr^500 title_de^500 title_es^500 content_fr^100 content_en^100 content_de^100 content_es^100 url_search^30","hl.maxAnalyzedChars":51200}}}'
-  	curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "300000"}}' $url_protocol://${ip_solr}/solr/$1/config
+  	curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"supdateHandler.autoCommit.maxTime": "300000"}}' $url_protocol://${ip_solr}/solr/$1/config
+   
     cd ${SOLR_INSTALL_DIR}/solrcloud/FileShare/conf/customs_schema && bash addCustomSchemaInfo.sh
 
     echo "Creation of Solr Collections"
@@ -55,18 +56,18 @@ curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-propert
 curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"duplicates.quant.rate": "0.1"}}' $url_protocol://${ip_solr}/solr/Duplicates/config
 
 
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/Statistics/config
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/Promolink/config
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/Access/config
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/Monitoring/config
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/Duplicates/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/Statistics/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/Promolink/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/Access/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/Monitoring/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/Duplicates/config
 
   curl -XGET --insecure "$url_protocol://${ip_solr}/solr/admin/collections?action=CREATE&name=OCR&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
   curl -XGET --insecure "$url_protocol://${ip_solr}/solr/admin/collections?action=CREATE&name=Spacy&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
   curl -XGET --insecure "$url_protocol://${ip_solr}/solr/admin/collections?action=CREATE&name=GenericAnnotator&collection.configName=GenericAnnotator&numShards=1&maxShardsPerNode=1&replicationFactor=1&property.lib.path=${SOLR_INSTALL_DIR}/solrcloud/GenericAnnotator/"
   
   
-  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-user-property": {"solr.autoCommit.maxTime": "60000"}}' $url_protocol://${ip_solr}/solr/GenericAnnotator/config
+  curl -XPOST --insecure -H 'Content-type:application/json' -d '{"set-property": {"updateHandler.autoCommit.maxTime": "600000"}}' $url_protocol://${ip_solr}/solr/GenericAnnotator/config
   
 
   
