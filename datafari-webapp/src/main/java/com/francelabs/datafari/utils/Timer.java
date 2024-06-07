c5dc07c042be192e51138e08d4b3e2428285d9a8
Emeric <emeric.bernet@francelabs.com>
2024-06-07 10:36:38 +0200

    612 - Make timers optional

--- datafari-webapp/src/main/java/com/francelabs/datafari/utils/Timer.java
+++ datafari-webapp/src/main/java/com/francelabs/datafari/utils/Timer.java
@@ -12,26 +12,35 @@ public class Timer {
     long endTime;
     String originClass;
     String originMethod;
+    boolean enabled = false;
 
     public Timer(String originClass, String originMethod) {
-        start();
-        this.originMethod = originMethod;
-        this.originClass = originClass;
+        final DatafariMainConfiguration config = DatafariMainConfiguration.getInstance();
+        if (config.getProperty(DatafariMainConfiguration.ENABLE_MONITORING_TIMER).equals("true")) {
+            enabled = true;
+            start();
+            this.originMethod = originMethod;
+            this.originClass = originClass;
+        }
     }
 
     public void start() {
-        startTime = System.currentTimeMillis ();
+        if (enabled) startTime = System.currentTimeMillis ();
     }
 
     public void top(String position) {
-        endTime = System.currentTimeMillis ();
-        long totalTime = endTime - startTime;
-        logger.info("Monitoring class {}. Execution of method {} in progress. Top for position {}: {} ms", originClass, originMethod, position, totalTime);
+        if (enabled) {
+            endTime = System.currentTimeMillis ();
+            long totalTime = endTime - startTime;
+            logger.info("Monitoring class {}. Execution of method {} in progress. Top for position {}: {} ms", originClass, originMethod, position, totalTime);
+        }
     }
 
     public void stop() {
-        endTime = System.currentTimeMillis ();
-        long totalTime = endTime - startTime;
-        logger.info("Monitoring class {}. Execution of method {} took {} ms", originClass, originMethod, totalTime);
+        if (enabled) {
+            endTime = System.currentTimeMillis ();
+            long totalTime = endTime - startTime;
+            logger.info("Monitoring class {}. Execution of method {} took {} ms", originClass, originMethod, totalTime);
+        }
     }
 }
