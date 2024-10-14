7d4a505d13088c63cc693fd7857b74ba8f155584
otavard <gitoliv@francelabs.com>
2024-10-14 10:30:39 +0200

    Add vectorMain collection and dedicated update processor

---
+++ datafari-updateprocessor/src/main/java/com/francelabs/datafari/updateprocessor/VectorUpdateProcessor.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ /*******************************************************************************
+ *  * Copyright 2020 France Labs
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *******************************************************************************/
+package com.francelabs.datafari.updateprocessor;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.update.AddUpdateCommand;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VectorUpdateProcessor extends UpdateRequestProcessor {
+  private static final Logger LOGGER = LogManager.getLogger(VectorUpdateProcessor.class.getName());
+  boolean enabled = false;
+
+  public VectorUpdateProcessor(final SolrParams params, final UpdateRequestProcessor next) {
+    super(next);
+    if (params != null) {
+      boolean enabled = params.getBool("enabled", false);
+      this.enabled = enabled;
+    }
+  }
+
+  @Override
+  public void processAdd(final AddUpdateCommand cmd) throws IOException {
+    if (enabled) {
+      final SolrInputDocument doc = cmd.getSolrInputDocument();
+
+      LOGGER.info("Vector Update processor");
+      
+
+
+
+      // VERY IMPORTANT ! without this line of code any other Update Processor declared AFTER this one in the conf WILL NOT EXECUTE
+      super.processAdd(cmd);
+    }
+  }
+}
