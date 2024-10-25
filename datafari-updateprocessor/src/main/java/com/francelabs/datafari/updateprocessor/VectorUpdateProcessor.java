5e79d97fbc80c394e49dc6fe250e4749c092205c
Emeric <emeric.bernet@francelabs.com>
2024-10-25 11:40:18 +0200

    Creation of Vector Update Processor

--- datafari-updateprocessor/src/main/java/com/francelabs/datafari/updateprocessor/VectorUpdateProcessor.java
+++ datafari-updateprocessor/src/main/java/com/francelabs/datafari/updateprocessor/VectorUpdateProcessor.java
@@ -18,24 +18,42 @@ package com.francelabs.datafari.updateprocessor;
 
 import java.io.IOException;
 
+import java.util.Arrays;
+import java.util.List;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.splitter.DocumentByParagraphSplitter;
+
+import dev.langchain4j.data.segment.TextSegment;
+
+
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.update.AddUpdateCommand;
 import org.apache.solr.update.processor.UpdateRequestProcessor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class VectorUpdateProcessor extends UpdateRequestProcessor {
   private static final Logger LOGGER = LogManager.getLogger(VectorUpdateProcessor.class.getName());
   boolean enabled = false;
+  CloudSolrClient client;
 
-  public VectorUpdateProcessor(final SolrParams params, final UpdateRequestProcessor next) {
+  private static final int CHUNK_SIZE = 20000;
+  private static final int MAX_OVERLAP_SIZE = 200;
+  private static final int VECTOR_DIMENSION = 4;
+
+  public VectorUpdateProcessor(CloudSolrClient client, final SolrParams params, final UpdateRequestProcessor next) {
     super(next);
     if (params != null) {
       boolean enabled = params.getBool("enabled", false);
       this.enabled = enabled;
+      this.client = client;
     }
   }
 
@@ -45,7 +63,56 @@ public class VectorUpdateProcessor extends UpdateRequestProcessor {
       final SolrInputDocument doc = cmd.getSolrInputDocument();
 
       LOGGER.info("Vector Update processor");
-      
+
+      String content = "";
+      final SolrInputField contentFieldFr = doc.get("content_fr");
+      final SolrInputField contentFieldEn = doc.get("content_en");
+      if (contentFieldFr != null) {
+        content = (String) contentFieldFr.getFirstValue();
+      } else if (contentFieldEn != null) {
+        content = (String) contentFieldEn.getFirstValue();
+      }
+
+
+      if (content != null && !content.isEmpty()) {
+        LOGGER.info(content);
+
+        // Chunking
+        DocumentSplitter splitter = new DocumentByParagraphSplitter(CHUNK_SIZE, MAX_OVERLAP_SIZE);
+        Document document = new Document(content);
+        List<TextSegment> chunks = splitter.split(document);
+        String parentId = (String) doc.get("id").getValue();
+
+        deleteExistingChildren(client, parentId);
+
+        for (TextSegment chunk : chunks) {
+          if(chunk != null && !chunk.text().isEmpty()) {
+            LOGGER.debug(chunk.text());
+            SolrInputDocument vectorDocument = new SolrInputDocument();
+
+            List<Float> vector = vectorEmbeddings(chunk.text());
+
+            if (chunk.text() != null && vector.size() == VECTOR_DIMENSION) {
+
+              String id = doc.get("id") + "_" + chunks.indexOf(chunk);
+              vectorDocument.addField("id", id);
+              vectorDocument.addField("vector", vector);
+              vectorDocument.addField("parent_doc", parentId);
+              vectorDocument.addField("content", chunk.text());
+
+              try {
+                client.add(vectorDocument);
+                client.commit();
+              } catch (SolrServerException e) {
+                LOGGER.warn("Warning : the document assiciated to the chunk {} could not be added.", id);
+              }
+            }
+          } else {
+            LOGGER.warn("The file {} appears to be empty and has been ignored during vector embeddings.", doc.get("id").getValue());
+          }
+        }
+
+      }
 
 
 
@@ -53,4 +120,32 @@ public class VectorUpdateProcessor extends UpdateRequestProcessor {
       super.processAdd(cmd);
     }
   }
+
+  private void deleteExistingChildren(CloudSolrClient client, String parentId) {
+    try {
+      client.deleteByQuery("parent_doc:\"" + parentId + "\"");
+      client.commit();
+    } catch (SolrServerException|IOException e) {
+      LOGGER.error("Could not delete existing children for this document");
+    }
+  }
+
+  private List<Float> vectorEmbeddings(String content) {
+    // TODO : This method should be edited to implement the Solr Embeddings Model
+
+    /*
+    SolrEmbeddingModel embedder = modelStore.getModel(embeddingModelName);
+    float[] vectorToSearch = embedder.vectorise(qstr);
+     */
+
+    /*
+    List<Float> vector = new ArrayList<>();
+    for (float f : vectorToSearch) {
+      vector.add(f);
+    }*/
+    List<Float> vector = Arrays.asList(0.25f , 4.1f, 3.7f, 5.1f);
+
+    return vector;
+  }
+
 }
--- datafari-updateprocessor/src/main/java/com/francelabs/datafari/updateprocessor/VectorUpdateProcessorFactory.java
+++ datafari-updateprocessor/src/main/java/com/francelabs/datafari/updateprocessor/VectorUpdateProcessorFactory.java
@@ -15,6 +15,10 @@
  *******************************************************************************/
 package com.francelabs.datafari.updateprocessor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.request.SolrPing;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.request.SolrQueryRequest;
@@ -22,24 +26,72 @@ import org.apache.solr.response.SolrQueryResponse;
 import org.apache.solr.update.processor.UpdateRequestProcessor;
 import org.apache.solr.update.processor.UpdateRequestProcessorFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
 public class VectorUpdateProcessorFactory extends UpdateRequestProcessorFactory {
 
 
+  private static final Logger LOGGER = LogManager.getLogger(VectorUpdateProcessorFactory.class.getName());
+
+  private static final String DEFAULT_COLLECTION = "VectorMain";
+  private static final String DEFAULT_HOST = "localhost:2181";
+  private static final String COLLECTION = "collection";
+  private static final String HOST = "host";
+
   private SolrParams params = null;
+  private CloudSolrClient client;
 
   @Override
   public void init(final NamedList args) {
     // Retrieve the parameters if any
     if (args != null) {
       params = args.toSolrParams();
-     
     }
   }
 
   @Override
   public UpdateRequestProcessor getInstance(final SolrQueryRequest req, final SolrQueryResponse rsp, final UpdateRequestProcessor next) {
     // Pass the parameters retrieved in the init (if any) to the update processor
-    return new VectorUpdateProcessor(params, next);
+    if (params.getBool("enabled", false) && tryInitClient()) {
+      return new VectorUpdateProcessor(client, params, next);
+    } else {
+      // Dummy request processor forwarding the processing to the rest of the pipe when text tagger is disabled.
+      return new UpdateRequestProcessor(next) {
+      };
+    }
+  }
+
+  private boolean tryInitClient() {
+    if (client == null) {
+      try {
+        client = new CloudSolrClient.Builder(new ArrayList<>(Collections.singletonList(getParam(HOST, DEFAULT_HOST))), Optional.empty())
+                .withDefaultCollection(getParam(COLLECTION, DEFAULT_COLLECTION))
+                .build();
+        final SolrPing ping = new SolrPing();
+        client.request(ping);
+        return true;
+      } catch (final Exception e) {
+        LOGGER.warn("{} error initializing the solr client to get tags, disabling text tagger.", e.getMessage());
+        client = null;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Extracts a String parameter from the SolrParams and return it if it exists.
+   * Sets it to its default value if it doesn't exist
+   *
+   * @param paramName the name of the parameter to set
+   * @param def       the default value to use
+   * @return the new value for the parameter, the default value is returned when the parameter is malformed and the tagger is disabled.
+   */
+  private String getParam(final String paramName, final String def) {
+    String result = params.get(paramName);
+    return (result != null && !result.isEmpty()) ? result : def;
   }
 
 }
