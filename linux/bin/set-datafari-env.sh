f1ecfba8f81f37b4966f64303946bc405e91003e
guylaine-bassette <guylaine.bassette@francelabs.com>
2026-04-13 13:14:23 +0200

    Updates log4j version for Tomcat mcf and webapp.
    https://gitlab.datafari.com/datafari-enterprise/datafariee/-/issues/676
    
    Remove the "bin-mcf" directory and its "setenv.sh" file, as they have been moved in  datafari-tomcat-mcf.

--- linux/bin/set-datafari-env.sh
+++ linux/bin/set-datafari-env.sh
@@ -54,7 +54,7 @@ export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=${TRUSTSTORE_PATH} -Dj
 export INIT_STATE_FILE=${DATAFARI_HOME}/bin/common/init_state.properties
 export CONFIG_FILE=${DATAFARI_HOME}/tomcat/conf/datafari.properties
 export SOLR_CONFIG_FILE=${DATAFARI_HOME}/tomcat/conf/solr.properties
-export LOG4J_VERSION=2.23.1
+export LOG4J_VERSION=2.24.3
 export LD_LIBRARY_PATH=${DATAFARI_HOME}/pgsql/lib
 export TMP_DIR=/tmp
 export CASSANDRA_JDK_UNSUPPORTED=true
