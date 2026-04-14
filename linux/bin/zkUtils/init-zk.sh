7412047c70aa6c61b0169a95928481f64ac59abd
olivier <gitoliv@francelabs.com>
2026-04-14 14:12:53 +0000

    Fix init zk for multiservers for EE

--- linux/bin/zkUtils/init-zk.sh
+++ linux/bin/zkUtils/init-zk.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
-#
-#
-# Send configuration to Zookeeper
-#
-#
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
@@ -16,20 +11,23 @@ nodes(){
         numrunningsolrnodes="`curl --silent 'http://localhost:8080/datafari-solr/zookeeper?detail=true&path=/live_nodes' | ${DATAFARI_HOME}/command/jq .znode.prop.children_count`"
 }
 
-NUMSHARDS="`echo ${NUMSHARDS} | tr -d '\r'`"
+NUMSHARDS="$(echo ${NUMSHARDS} | tr -d '\r')"
+
+SOLR_ZK_CLI="$DATAFARI_HOME/solr/bin/solr"
 
 echo "Uploading configuration to zookeeper"
+
 # Start SSL configuration
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -zkhost @SOLRHOSTS@ -cmd clusterprop -name urlScheme -val https
-# End SSL configuration
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Duplicates/conf" -confname Duplicates
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Entities/conf" -confname Entities
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/FileShare/conf" -confname Init
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Statistics/conf" -confname Statistics
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Promolink/conf" -confname Promolink
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Access/conf" -confname Access
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Monitoring/conf" -confname Monitoring
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Crawl/conf" -confname Crawl
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/Logs/conf" -confname Logs
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/GenericAnnotator/conf" -confname GenericAnnotator
-"${DATAFARI_HOME}/solr/server/scripts/cloud-scripts/zkcli.sh" -cmd upconfig -zkhost @SOLRHOSTS@ -confdir "${DATAFARI_HOME}/solr/solrcloud/VectorMain/conf" -confname VectorMain
+"${DATAFARI_HOME}/solr/bin/solr" cluster --property urlScheme --value https --zk-host @SOLRHOSTS@
+# Upload configs
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Duplicates/conf" -n Duplicates
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Entities/conf" -n Entities
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/FileShare/conf" -n Init
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Statistics/conf" -n Statistics
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Promolink/conf" -n Promolink
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Access/conf" -n Access
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Monitoring/conf" -n Monitoring
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Crawl/conf" -n Crawl
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/Logs/conf" -n Logs
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/GenericAnnotator/conf" -n GenericAnnotator
+"$SOLR_ZK_CLI" zk upconfig -z @SOLRHOSTS@ -d "${DATAFARI_HOME}/solr/solrcloud/VectorMain/conf" -n VectorMain
