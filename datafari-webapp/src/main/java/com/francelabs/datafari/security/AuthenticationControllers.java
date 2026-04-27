047d27e02d1be280573d24eaae0d83c62b846711
guylaine-bassette <guylaine.bassette@francelabs.com>
2026-04-27 13:51:55 +0200

    EE#682: Update to Spring Security 7.x: First compilation with Boot 4 and Security 7.0.4 with all mechanisms tested OK, except Kerberos.
    https://gitlab.datafari.com/datafari-enterprise/datafariee/-/issues/682

--- datafari-webapp/src/main/java/com/francelabs/datafari/security/AuthenticationControllers.java
+++ datafari-webapp/src/main/java/com/francelabs/datafari/security/AuthenticationControllers.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
