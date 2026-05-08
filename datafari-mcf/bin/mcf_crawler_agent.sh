1edf8b4e729ea370b8d2b043c63b7263b6c7fafb
otavard <gitoliv@francelabs.com>
2026-05-08 15:21:07 +0200

    Minor fix : change message mcf agent stop

--- datafari-mcf/bin/mcf_crawler_agent.sh
+++ datafari-mcf/bin/mcf_crawler_agent.sh
@@ -29,7 +29,7 @@ cmd_stop() {
     mcf_pid=$(cat $MCF_PID_FILE)
     if ps -p $mcf_pid > /dev/null
     then
-       echo "Could not stop the MCF crawler agent process ! It's PID file will not be removed !"
+       echo "Could not stop the MCF crawler agent process ! Its PID file will not be removed !"
     else
       rm -f $MCF_PID_FILE
     fi
