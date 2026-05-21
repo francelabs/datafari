75a7d2e275cb9ed3d4cb7e4d65a328d172db902b
Emeric <emeric.bernet@francelabs.com>
2026-05-21 17:29:15 +0200

    [Binary] Fix docling in Binary Connector

--- datafari-binary-connector/src/main/java/com/francelabs/datafari/transformation/binary/services/DoclingExternalService.java
+++ datafari-binary-connector/src/main/java/com/francelabs/datafari/transformation/binary/services/DoclingExternalService.java
@@ -12,13 +12,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.UUID;
 
 public class DoclingExternalService extends ExternalService implements IExternalService {
 
     private static final Logger LOGGER = LogManager.getLogger(DoclingExternalService.class.getName());
 
-    static final String DEFAULT_URL = "http://http://127.0.0.1:5001";
+    static final String DEFAULT_URL = "http://127.0.0.1:5001";
     static final String DEFAULT_ENDPOINT = "/v1/convert/source";
 
     public DoclingExternalService(BinarySpecification spec) {
@@ -38,19 +40,26 @@ public class DoclingExternalService extends ExternalService implements IExternal
         if (apiToken == null || apiToken.isBlank()) {
             throw new ManifoldCFException("Unable to retrieve security token.");
         }
-        HttpClient client = HttpClient.newHttpClient();
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
         String filename = UUID.randomUUID().toString();
 
-        String requestBody = "{\"options\": {\"from_formats\": [\"docx\", \"pptx\", \"html\", \"image\", \"pdf\", \"asciidoc\", \"md\", \"xlsx\"],\"to_formats\": [\"md\", \"json\"],\"image_export_mode\": \"placeholder\",\"do_ocr\": true,\"force_ocr\": false,\"ocr_lang\": [\"en\", \"fr\", \"es\", \"de\"]},\"http_sources\": [{\"base64_string\": \"${B64_DATA}\",\"filename\": \"${FILENAME}\",\"kind\": \"file\"}]}"
+        // Remove prefix
+        base64content = base64content.replaceFirst("^data:.*;base64,", "");
+
+        String requestBody = "{\"options\": {\"from_formats\": [\"docx\", \"pptx\", \"html\", \"image\", \"pdf\", \"asciidoc\", \"md\", \"xlsx\"],\"to_formats\": [\"md\", \"json\"],\"image_export_mode\": \"placeholder\",\"do_ocr\": true,\"force_ocr\": false,\"ocr_lang\": [\"en\", \"fr\", \"es\", \"de\"]},\"sources\": [{\"base64_string\": \"${B64_DATA}\",\"filename\": \"${FILENAME}\",\"kind\": \"file\"}]}"
             .replace("${B64_DATA}", base64content)
             .replace("${FILENAME}", filename); // TODO : retrieve actual filename
         
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(this.url)
+                .version(HttpClient.Version.HTTP_1_1)
                 .header("X-Api-Key", apiToken)
-                .header("accept", "application/json")
+                .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody, StandardCharsets.UTF_8))
                 .build();
 
         HttpResponse<String> response;
